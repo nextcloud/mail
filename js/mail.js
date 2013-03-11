de879919b68de0215266a9f727c6badc8b3584fd
VicDeo <victor.dubiniuk@gmail.com>
2013-03-11 22:47:56 +0400

    Error handler

--- js/mail.js
+++ js/mail.js
@@ -260,7 +260,13 @@ $(document).ready(function () {
                     }
                     OC.dialogs.alert(error, t('mail', 'Error'));
                 }
-            }
+            },
+			error: function(jqXHR, textStatus, errorThrown){
+				var error = errorThrown || textStatus || t('mail', 'Unknown error');
+				$('#auto_detect_account').attr('disabled', 'false');
+				$('#connect-loading').fadeOut();
+				OC.dialogs.alert(error, t('mail', 'Server Error'));
+			}
         });
     });
 
