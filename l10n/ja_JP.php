f76e991e55a7ee6a37fbcea1d185d2d634e50a47
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-04-17 02:48:37 +0200

    [tx-robot] updated from transifex

--- l10n/ja_JP.php
+++ l10n/ja_JP.php
@@ -1,7 +1,15 @@
 <?php $TRANSLATIONS = array(
 "Other" => "その他",
 "Error" => "エラー",
+"Connecting ..." => "接続中...",
 "Connect" => "接続",
+"Not a email address" => "メールアドレスではありません",
+"New Message" => "新しいメッセージ",
 "To" => "終了",
-"Send" => "送信"
+"Subject" => "件名",
+"Send" => "送信",
+"Reply" => "返信",
+"Connect your mail account" => "メールアカウントに接続",
+"Mail Address" => "メールアドレス",
+"IMAP Password" => "IMAP パスワード"
 );
