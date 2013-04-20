c9e80e6f0b7a5a955f3d11bf9ee7e1cde9af4929
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-04-20 02:42:28 +0200

    [tx-robot] updated from transifex

--- l10n/sk_SK.php
+++ l10n/sk_SK.php
@@ -1,4 +1,10 @@
 <?php $TRANSLATIONS = array(
+"Not Found" => "Nenájdené",
+"Permission Denied" => "Prístup odmietnutý",
+"Authentication failure." => "Chybné overenie.",
+"The server does not support the %s extension." => "Server nepodporuje rozlíšenie %s.",
+"%s not supported on POP3 servers." => "%s nie je podporovaný na POP3 serveroch.",
+"Cannot convert search query text to new charset" => "Nemožno previesť vyhľadávanie na novú znakovú sadu",
 "Other" => "Iné",
 "Error" => "Chyba",
 "Connect" => "Pripoj",
