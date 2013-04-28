d6ca15a7a46c0f9060f7e627291b246ce30b952b
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-04-28 02:42:22 +0200

    [tx-robot] updated from transifex

--- l10n/hu_HU.php
+++ l10n/hu_HU.php
@@ -1,4 +1,10 @@
 <?php $TRANSLATIONS = array(
+"Not Found" => "Nem talált",
+"Permission Denied" => "Hozzáférés megtagadva",
+"Honduras" => "Honduras",
+"Hong Kong" => "Hong Kong",
+"Hungary" => "Magyarország",
+"India" => "India",
 "Other" => "Más",
 "Error" => "Hiba",
 "Connect" => "Kapcsolódás",
