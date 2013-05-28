9c83dd2223d2f59b1606574335efa738e0c40fc6
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-28 02:43:02 +0200

    [tx-robot] updated from transifex

--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -63,7 +63,17 @@
 "HTML Version of Message" => "郵件的 HTML 版本",
 "Other" => "其他",
 "Error" => "錯誤",
+"Connecting ..." => "連線中...",
 "Connect" => "連線",
+"Not a email address" => "不是合法的電子郵件地址",
+"Unknown error" => "未知的錯誤",
+"Server Error" => "伺服器錯誤",
+"New Message" => "新郵件",
 "To" => "至",
-"Send" => "寄出"
+"Subject" => "主旨",
+"Send" => "寄出",
+"Connect your mail account" => "連結您的郵件帳戶",
+"Mail Address" => "電子郵件地址",
+"IMAP Password" => "IMAP 密碼",
+"Reply" => "回覆"
 );
