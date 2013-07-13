35e4f9adef6eb76875896a5e1a747109ee345767
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-13 03:53:14 +0200

    [tx-robot] updated from transifex

--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -125,10 +125,12 @@
 "Connecting ..." => "A ligar...",
 "Connect" => "Ligar",
 "Unknown error" => "Erro Desconhecido",
+"Server Error" => "Erro no servidor",
 "New Message" => "Nova Mensagem",
 "To" => "Para",
 "Subject" => "Assunto",
 "Send" => "Enviar",
+"Connect your mail account" => "Ligar à sua conta de correio",
 "Mail Address" => "Endereço de email",
 "IMAP Password" => "Password IMAP",
 "Reply" => "Responder"
