d53e71ccfdaa22acb93b746149fd24d9f79c13e4
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-31 02:42:31 -0400

    [tx-robot] updated from transifex

--- l10n/zh_CN.GB2312.php
+++ l10n/zh_CN.GB2312.php
@@ -1,4 +1,11 @@
 <?php $TRANSLATIONS = array(
+"Not Found" => "未找到",
+"Permission Denied" => "权限不足",
+"Authentication failure." => "验证错误",
+"Cannot expunge read-only mailbox." => "无法删除只读邮箱",
+"The server does not support the %s extension." => "服务器不支持%s文件扩展",
+"%s not supported on POP3 servers." => "%s不被POP3服务器支持",
+"Cannot convert search query text to new charset" => "无法转换搜索内容的字符编码",
 "Other" => "其他",
 "Error" => "出错",
 "Connect" => "连接",
