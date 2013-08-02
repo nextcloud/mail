d909411249ee6223c5a592fa9f10870f99a713fb
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-08-02 02:42:32 -0400

    [tx-robot] updated from transifex

--- l10n/ms_MY.php
+++ l10n/ms_MY.php
@@ -1,4 +1,9 @@
 <?php $TRANSLATIONS = array(
+"Not Found" => "Tidak Dijumpai",
+"Error connecting to mail server." => "Ralat menyambung ke pelayan mel",
+"List-Owner" => "Senarai-Pemilik",
+"List-Post" => "Senarai-Pos",
+"List-Id" => "Senarai-Id",
 "Other" => "Lain",
 "Error" => "Ralat",
 "Connect" => "Berhubung",
