82bfdf5f9868fe425221e052fcfea24cd2114f39
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-10-02 12:04:03 -0400

    [tx-robot] updated from transifex

--- l10n/ar.php
+++ l10n/ar.php
@@ -1,8 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Not Found" => "لم يتم العثور عليه",
-"Permission Denied" => "تم رفض الوصول",
-"Authentication failure." => "فشل في التصديق",
+"Permission Denied" => "تم رفض الإذن",
+"Authentication failure." => "فشل في اثباث الهوية",
 "Cannot expunge read-only mailbox." => "لا يمكن محو صندوق بريد ذو خاصية \"للقراءة فقط\"",
+"The server does not support the %s extension." => "الموزع لا يدعم هذه الخاصية  %s ",
+"%s not supported on POP3 servers." => "POP3 غير مدعومة من موزع %s",
 "Cannot convert search query text to new charset" => "لا يمكن تحويل نص استعلام بحث إلى محارف جديدة",
 "Bad tagged response." => "استجابة سيئة العنونة",
 "Other" => "شيء آخر",
