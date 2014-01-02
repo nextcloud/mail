49f6ae6ddc74dcf98d126a17494de536cc81634a
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-02 02:42:35 -0500

    [tx-robot] updated from transifex

---
+++ l10n/es_MX.php
@@ -0,0 +1,17 @@
+<?php $TRANSLATIONS = array(
+"Error" => "Error",
+"Delete message" => "Eliminar mensaje",
+"Connecting ..." => "Conectando...",
+"Connect" => "Conectar",
+"Not a email address" => "No es una dirección de correo",
+"Unknown error" => "Error desconocido",
+"Server Error" => "Error del Servidor",
+"New Message" => "Nuevo Mensaje",
+"To" => "Hasta",
+"Subject" => "Asunto",
+"Send" => "Enviar",
+"Connect your mail account" => "Conecte su cuenta de correo",
+"Mail Address" => "Dirección de correo",
+"IMAP Password" => "Contraseña IMAP",
+"Reply" => "Responder"
+);
--- l10n/id.php
+++ l10n/id.php
@@ -1,7 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "Galat",
+"Delete message" => "Hapus Pesan",
+"Connecting ..." => "Menghubungkan ...",
 "Connect" => "Sambungkan",
+"Not a email address" => "Bukan sebuah alamat email",
 "Unknown error" => "Galat tidak diketahui",
+"Server Error" => "Kesalahan Server",
+"New Message" => "Pasan Baru",
 "To" => "Kepada",
-"Send" => "Kirim"
+"Subject" => "Judul",
+"Send" => "Kirim",
+"Connect your mail account" => "Hubungkan akun email anda",
+"Mail Address" => "Alamat Email",
+"IMAP Password" => "Sandi IMAP",
+"Reply" => "Balas"
 );
