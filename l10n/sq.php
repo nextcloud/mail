57e215c1a8153cac471a76a4470830470d78371f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-18 02:41:55 -0500

    [tx-robot] updated from transifex

--- l10n/sq.php
+++ l10n/sq.php
@@ -1,5 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "Veprim i gabuar",
+"Delete message" => "Fshi mesazhin",
+"Connecting ..." => "Duke u lidhur...",
+"Connect" => "Lidhu",
+"Not a email address" => "Jo një adrese email-i",
+"Unknown error" => "Gabim panjohur",
+"Server Error" => "Gabim N'server",
+"New Message" => "Mesazh i'ri",
 "To" => "Për",
-"Send" => "Dërgo"
+"Subject" => "Subjekti",
+"Send" => "Dërgo",
+"Connect your mail account" => "Lidhni llogarinë tuaj të mail-it",
+"Mail Address" => "Adresa Mail-it",
+"IMAP Password" => "Fjalëkalimi IMAP",
+"Reply" => "Përgjigju"
 );
--- l10n/vi.php
+++ l10n/vi.php
@@ -1,7 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "Lỗi",
+"Delete message" => "Xóa th",
+"Connecting ..." => "Đang kết nối...",
 "Connect" => "Kết nối",
+"Not a email address" => "Đây không phải là một địa chỉ thư điện tử",
 "Unknown error" => "Lỗi chưa biết",
+"Server Error" => "Máy chủ bị l",
+"New Message" => "Tin m",
 "To" => "Tới",
-"Send" => "Gởi"
+"Subject" => "Tiêu ",
+"Send" => "Gởi",
+"Connect your mail account" => "Kết nối tới hòm thư của bạn",
+"Mail Address" => "Địa chỉ hòm th",
+"IMAP Password" => "Mật khẩu ",
+"Reply" => "Trả l"
 );
