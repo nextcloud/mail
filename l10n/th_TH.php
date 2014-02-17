77ed6519b471357d8218761d56b9fe7204604255
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-17 04:17:45 -0500

    [tx-robot] updated from transifex

--- l10n/th_TH.php
+++ l10n/th_TH.php
@@ -1,6 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Error" => "ข้อผิดพลาด",
+"Connecting ..." => "กำลังเชื่อมต่อ ...",
 "Connect" => "เชื่อมต่อ",
+"Unknown error" => "ข้อผิดพลาดที่ไม่ทราบสาเหตุ",
+"New Message" => "ข้อความใหม่",
 "To" => "ถึง",
-"Send" => "ส่ง"
+"Send" => "ส่ง",
+"Reply" => "ตอบกลับ"
 );
