fcd1f98826c0f3e5d2ed94bc6752ad3b7e3d0bf1
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-21 02:42:18 -0500

    [tx-robot] updated from transifex

--- l10n/ar.php
+++ l10n/ar.php
@@ -1,7 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "خطأ",
+"Delete message" => "حذف الرسالة",
+"Connecting ..." => "جاري الاتصال ...",
 "Connect" => "اتصال",
+"Not a email address" => "ليس بريد الكتروني",
 "Unknown error" => "حدث خطأ غير معروف. ",
+"Server Error" => "خطا في الخادم",
+"New Message" => "رسالة جديدة",
 "To" => "إلى",
-"Send" => "أرسل"
+"Subject" => "الموضوع",
+"Send" => "أرسل",
+"Connect your mail account" => "اتصل بـ حساب بريدك الالكتروني",
+"Mail Address" => "عنوان البريد",
+"IMAP Password" => "كلمة المرور الخاصة بـ IMAP",
+"Reply" => "رد"
 );
