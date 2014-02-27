d42b22aacc72e2ef4dc0d10d9409f0da9499d2dd
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-27 02:42:04 -0500

    [tx-robot] updated from transifex

--- l10n/km.php
+++ l10n/km.php
@@ -1,4 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "កំហុស",
-"To" => "ទៅ"
+"Delete message" => "លុប​សារ",
+"Connecting ..." => "កំពុង​ភ្ជាប់ ...",
+"Connect" => "ភ្ជាប់",
+"Not a email address" => "មិន​មែន​ជា​អាសយដ្ឋាន​អ៊ីមែល​ទេ",
+"Unknown error" => "មិន​ស្គាល់​កំហុស",
+"Server Error" => "កំហុស​ម៉ាស៊ីន​បម្រើ",
+"New Message" => "សារ​ថ្មី",
+"To" => "ទៅ",
+"Subject" => "ប្រធានបទ",
+"Send" => "ផ្ញើ",
+"Connect your mail account" => "ភ្ជាប់​គណនី​អ៊ីមែល​របស់​អ្នក",
+"Mail Address" => "អាសយដ្ឋាន​អ៊ីមែល",
+"IMAP Password" => "ពាក្យ​សម្ងាត់ IMAP",
+"Reply" => "ឆ្លើយតប"
 );
