99181724327db19d939851e7680e1d66c20101a6
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-03-12 02:41:39 -0400

    [tx-robot] updated from transifex

--- l10n/ro.php
+++ l10n/ro.php
@@ -1,6 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "Eroare",
+"Delete message" => "Șterge mesajul",
+"Connecting ..." => "Se conectează...",
 "Connect" => "Conectare",
+"Not a email address" => "Nu este o adresă de email",
+"Unknown error" => "Eroare necunoscută",
+"Server Error" => "Eroare de server",
+"New Message" => "Mesaj nou",
 "To" => "Către",
-"Send" => "Expediază"
+"Subject" => "Subiect",
+"Send" => "Expediază",
+"Connect your mail account" => "Conectează contul tău de email",
+"Mail Address" => "Adresă de email",
+"IMAP Password" => "Parolă IMAP",
+"Reply" => "Răspunde"
 );
