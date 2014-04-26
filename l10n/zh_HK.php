720511cfded90e8df84d7a2aa39add4f2a35b39a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-04-26 02:41:44 -0400

    [tx-robot] updated from transifex

--- l10n/zh_HK.php
+++ l10n/zh_HK.php
@@ -1,5 +1,12 @@
 <?php $TRANSLATIONS = array(
 "Error" => "錯誤",
+"Connecting ..." => "連接中",
 "Connect" => "連線",
-"Send" => "傳送"
+"Not a email address" => "不是電郵地址",
+"New Message" => "新訊息",
+"Subject" => "標題",
+"Send" => "傳送",
+"Connect your mail account" => "連接您的電郵帳號",
+"Mail Address" => "電郵地址",
+"Reply" => "回覆"
 );
