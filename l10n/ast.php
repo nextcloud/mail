b1562758204e55c367e81317ac253f4340d47f0e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-02 02:42:19 -0400

    [tx-robot] updated from transifex

--- l10n/ast.php
+++ l10n/ast.php
@@ -1,7 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Error" => "Fallu",
+"Connecting ..." => "Coneutando...",
 "Connect" => "Coneutar",
 "Unknown error" => "Fallu desconocíu",
+"New Message" => "Mensaxe nuevu",
 "To" => "Hasta",
 "Send" => "Unviar"
 );
