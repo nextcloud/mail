eae4e6838dccd8a3f3c47d9d593bd3accba92d39
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-05-08 10:26:35 +0200

    remove infield labels

--- templates/no-accounts.php
+++ templates/no-accounts.php
@@ -2,14 +2,16 @@
 	<fieldset>
 		<legend><?php p($l->t('Connect your mail account')) ?></legend>
 
-		<p class="infield grouptop">
-			<input type="text" name="mail-address" id="mail-address" placeholder=""
-				   value="" autofocus autocomplete="off" required/>
+		<p class="grouptop">
+			<input type="text" name="mail-address" id="mail-address"
+				placeholder="<?php p($l->t('Mail Address')); ?>" value=""
+				autofocus autocomplete="off" required/>
 			<label for="mail-address" class="infield"><?php p($l->t('Mail Address')); ?></label>
 		</p>
 
-		<p class="infield groupbottom">
-			<input type="password" name="mail-password" id="mail-password" value="" placeholder="" />
+		<p class="groupbottom">
+			<input type="password" name="mail-password" id="mail-password"
+				placeholder="<?php p($l->t('IMAP Password')); ?>" value="" />
 			<label for="mail-password" class="infield"><?php p($l->t('IMAP Password')); ?></label>
 		</p>
 		<img id="connect-loading" src="<?php print_unescaped(OCP\Util::imagePath('core', 'loading.gif')); ?>" style="display:none;" />
