61185d9dc3a581b1691d28937d236aa3ce4fb37f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-18 02:41:17 -0400

    [tx-robot] updated from transifex

--- l10n/ur_PK.php
+++ l10n/ur_PK.php
@@ -1,4 +1,17 @@
 <?php $TRANSLATIONS = array(
 "Error" => "ایرر",
-"Send" => "بھجیں"
+"Delete message" => "رسالہ حذف کریں",
+"Connecting ..." => "منسلک ہو رہا ہے",
+"Connect" => "منسلک",
+"Not a email address" => "ای میل ایڈریس نہیں",
+"Unknown error" => "غیر معروف خرابی",
+"Server Error" => "سرور کی خرابی",
+"New Message" => "جدید رسالہ",
+"To" => "طرف",
+"Subject" => "عنوان",
+"Send" => "بھجیں",
+"Connect your mail account" => "اپنے  میل اکاؤنٹ سے منسلک کریں",
+"Mail Address" => "میل ایڈریس",
+"IMAP Password" => "ای ایم ایے پی پاس ورڈ",
+"Reply" => "جواب"
 );
