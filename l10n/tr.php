8c2626ed8dd7294c019d84369ca52e0ffe77a0c5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-25 02:41:05 -0400

    [tx-robot] updated from transifex

--- l10n/tr.php
+++ l10n/tr.php
@@ -3,15 +3,15 @@
 "Delete message" => "Mesajı sil",
 "Connecting ..." => "Bağlanıyor ...",
 "Connect" => "Bağlan",
-"Not a email address" => "E-posta adresi değil",
+"Not a email address" => "Bir e-posta adresi değil",
 "Unknown error" => "Bilinmeyen hata",
 "Server Error" => "Sunucu Hatası",
 "New Message" => "Yeni Mesaj",
 "To" => "Kime",
 "Subject" => "Konu",
 "Send" => "Gönder",
-"Connect your mail account" => "E-posta hesabınızı bağlayın",
+"Connect your mail account" => "Posta hesabınızı bağlayın",
 "Mail Address" => "Posta Adresi",
 "IMAP Password" => "IMAP Parolası",
-"Reply" => "Cevap"
+"Reply" => "Yanıtla"
 );
