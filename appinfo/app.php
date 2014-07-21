dbb5f95e99f3ae91910ae47bf53e268f11336164
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-07-21 17:41:33 +0200

    make appname translatable

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,15 +3,17 @@
 use OCA\Mail\HordeTranslationHandler;
 
 require_once __DIR__ .  '/../vendor/autoload.php';
+$l = OC_L10N::get('mail');
 
 // bypass Horde Translation system
 Horde_Translation::setHandler('Horde_Imap_Client', new HordeTranslationHandler());
 
-OCP\App::addNavigationEntry( array(
+OCP\App::addNavigationEntry(array(
   'id' => 'mail_index',
   'order' => 1,
   'href' => OCP\Util::linkToRoute( 'mail.page.index' ),
   'icon' => OCP\Util::imagePath( 'mail', 'mail.svg' ),
-  'name' => 'Mail'));
+  'name' => $l->t('Mail'),
+));
 
 //OCP\App::registerPersonal('mail','settings');
