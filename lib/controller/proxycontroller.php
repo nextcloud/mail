ed5534f30235946166bd0b25dbf2f84591e5313b
Lukas Reschke <lukas@statuscode.ch>
2014-07-27 16:49:35 +0200

    Update PHP doc

--- lib/controller/proxycontroller.php
+++ lib/controller/proxycontroller.php
@@ -40,7 +40,7 @@ class ProxyController extends Controller {
 	/**
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
-	 *
+	 * @throws \Exception If the URL is not valid.
 	 * @return TemplateResponse
 	 */
 	public function redirect() {
