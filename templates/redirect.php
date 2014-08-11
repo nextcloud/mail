1d9aed9b231107f7714750932fbb44a57bd32bd5
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-08-11 15:34:31 +0200

    improve styling for link redirect page

--- templates/redirect.php
+++ templates/redirect.php
@@ -2,14 +2,16 @@
 <div id="app">
 	<div id="app-content">
 		<?php if (isset($_['authorizedRedirect']) && ($_['authorizedRedirect'])): ?>
-			<legend>
-				<p id="message" style="padding-top:25px;">
-					<img class="float-spinner" src="<?php p(\OCP\Util::imagePath('core', 'loading-dark.gif'));?>"/>
-					<a href="<?php p($_['url']) ?>" rel="noreferrer" id="redirectLink">
-						<span id="messageText"><?php p($l->t('Forwarding you to %s', array($_['urlHost'])));?></span>
+			<div class="error" style="text-align: center;">
+				<img src="<?php p(\OCP\Util::imagePath('core', 'loading-dark.gif'));?>"
+					style="margin: 0 auto;" />
+				<p>
+					<a href="<?php p($_['url']) ?>" rel="noreferrer" id="redirectLink"
+						style="color: #fff !important;">
+						<?php p($l->t('Forwarding you to %s', array($_['urlHost'])));?>
 					</a>
 				</p>
-			</legend>
+			</div>
 		<?php else: ?>
 			<div class="error">
 				<legend><strong><?php p($l->t('Redirect Warning'));?></strong></legend>
