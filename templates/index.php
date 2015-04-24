0204f4d0b5032b36fae45393a4ee19e28a37f106
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-04-24 15:46:12 +0200

    improve design of empty content message for folders

--- templates/index.php
+++ templates/index.php
@@ -229,7 +229,10 @@ script('mail', 'jquery.storageapi');
 </script>
 <script id="message-list-template" type="text/x-handlebars-template">
 	<input type="button" id="load-new-mail-messages" value="<?php p($l->t('Check messages …')); ?>">
-	<div id="emptycontent" style="display: none;"><?php p($l->t('No messages in this folder!')); ?></div>
+	<div id="emptycontent" style="display: none;">
+		<div class="icon-mail"></div>
+		<h2><?php p($l->t('No messages in this folder!')); ?></h2>
+	</div>
 	<div id="mail-message-list"></div>
 	<input type="button" id="load-more-mail-messages" value="<?php p($l->t('Load more …')); ?>">
 </script>
