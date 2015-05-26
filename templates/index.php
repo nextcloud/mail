b8aed4801b81d8eb4c41ab3750074b33a469e8c4
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-05-26 11:23:06 +0200

    show ellipsis on long mail addresses fixes #726

--- templates/index.php
+++ templates/index.php
@@ -37,7 +37,7 @@ script('mail', 'jquery-visibility');
 	</li>
 </script>
 <script id="mail-account-template" type="text/x-handlebars-template">
-	<h2 class="mail_account_email">{{email}}</h2>
+	<h2 class="mail-account-email" title="{{email}}">{{email}}</h2>
 	<ul id="mail_folders" class="mail_folders with-icon" data-account_id="{{id}}">
 	</ul>
 </script>
