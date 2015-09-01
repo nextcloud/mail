b8009568265d3f5a2c5031151fe6e25fa6930b04
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-09-01 16:53:01 +0200

    add jasmine/karma for js unit testing

---
+++ js/tests/TestSpec.js
@@ -0,0 +1,17 @@
+/**
+ * ownCloud - Mail
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @copyright Christoph Wurst 2015
+ */
+
+define(['models/account'], function(Account) {
+	describe("test nothing", function() {
+		it("should be able to initialize", function() {
+			expect(1).toEqual(10);
+		});
+	});
+});
