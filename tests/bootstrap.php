ce7364ecdaeb160e1a96b27b5d946eb416ed0ff0
Steffen Lindner <mail@steffen-lindner.de>
2015-09-02 15:41:18 +0200

    Add version check

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,10 @@ define('PHPUNIT_RUN', 1);
 
 require_once __DIR__.'/../../../lib/base.php';
 require_once __DIR__.'/../vendor/autoload.php';
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+
+if (version_compare(implode('.', \OCP\Util::getVersion()), '8.2', '<=')) {
+	\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+}
 
 if(!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
