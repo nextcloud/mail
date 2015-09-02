0cd7d3736fc7b6f3f5def47e79d3d73a9d9c754a
Jakob Sack <mail@jakobsack.de>
2015-09-03 01:05:11 +0200

    Add way to block (and recover) background images

---
+++ lib/service/htmlpurify/transformcssbackground.php
@@ -0,0 +1,73 @@
+<?php
+ /**
+ * ownCloud - Mail app
+ *
+ * @author Jakob Sack
+ * @copyright 2015 Jakob Sack jakob@owncloud.org
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+namespace OCA\Mail\Service\HtmlPurify;
+use HTMLPurifier_AttrTransform;
+use HTMLPurifier_Config;
+use HTMLPurifier_Context;
+use OCP\IURLGenerator;
+use OCP\Util;
+
+/**
+ * Adds copies src to data-src on all img tags.
+ */
+class TransformCSSBackground extends HTMLPurifier_AttrTransform {
+	/** @var IURLGenerator */
+	private $urlGenerator;
+
+	public function __construct(IURLGenerator $urlGenerator) {
+		$this->urlGenerator = $urlGenerator;
+	}
+
+	/**
+	 * @param array $attr
+	 * @param HTMLPurifier_Config $config
+	 * @param HTMLPurifier_Context $context
+	 * @return array
+	 */
+	public function transform($attr, $config, $context) {
+		if (!isset($attr['style']) ||
+			strpos($attr['style'], 'background') === false) {
+			return $attr;
+		}
+
+		// Check if there is a background image given
+		$cssAttributes = explode(';', $attr['style']);
+
+		$func = function($cssAttribute) {
+			list($name, $value) = explode(':', $cssAttribute, 2);
+			if(strpos($name, 'background') !== false &&
+				strpos($value, 'url(') !== false) {
+				// Replace image URL
+				$value = preg_replace('/url\("?http.*\)/i',
+					'url('.Util::imagePath('mail', 'blocked-image.png').')',
+					$value);
+				return $name.':'.$value;
+			} else {
+				return $cssAttribute;
+			}
+		};
+
+		// Reassemble style
+		$cssAttributes = array_map($func, $cssAttributes);
+		$new_style = implode(';', $cssAttributes);
+
+		// Replace style if required
+		if ($new_style !== $attr['style']) {
+			$attr['data-original-style'] = $attr['style'];
+			$attr['style'] = $new_style;
+		}
+
+		return $attr;
+	}
+}
