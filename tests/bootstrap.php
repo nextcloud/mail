4aed4699efc493be1652254ae8d5b779b98e542e
Olivier Paroz <oparoz@users.noreply.github.com>
2015-09-02 12:21:44 +0200

    Add tests to the list of allowed paths

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,7 @@ define('PHPUNIT_RUN', 1);
 
 require_once __DIR__.'/../../../lib/base.php';
 require_once __DIR__.'/../vendor/autoload.php';
+\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
 if(!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
