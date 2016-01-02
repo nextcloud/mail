8e213584361f90e4a5689fae350af11a65af657b
Christoph Wurst <wurst.christoph@gmail.com>
2016-01-02 12:31:33 +0100

    OC global is not used anymore, resolves jshint error

--- js/init.js
+++ js/init.js
@@ -1,5 +1,3 @@
-/* global OC */
-
 /**
  * ownCloud - Mail
  *
@@ -7,7 +5,7 @@
  * later. See the COPYING file.
  *
  * @author Christoph Wurst <christoph@winzerhof-wurst.at>
- * @copyright Christoph Wurst 2015
+ * @copyright Christoph Wurst 2015, 2016
  */
 
 define(function(require) {
