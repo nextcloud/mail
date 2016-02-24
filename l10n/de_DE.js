2309855e98e5b57d7dc2f5eb6b288269b0c65a0e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-24 15:14:19 -0500

    [tx-robot] updated from transifex

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -9,6 +9,7 @@ OC.L10N.register(
     "Message could not be starred. Please try again." : "Nachricht konnte nicht als Favorit markiert werden. Bitte noch einmal versuchen.",
     "Unknown error" : "Unbekannter Fehler",
     "Error while creating an account: " : "Fehler beim Erstellen eines Kontos:",
+    "Draft saved!" : "Entwurf gespeichert!",
     "Error while loading messages." : "Fehler beim Laden der Nachrichten.",
     "Choose a folder to store the attachment in" : "Ordner zum Speichern des Anhangs auswählen",
     "Attachments saved to Files." : "Anhänge wurde in Dateien gespeichert.",
@@ -18,7 +19,6 @@ OC.L10N.register(
     "Opening HTML drafts is not supported yet." : "Das Öffnen von HTML-Entwürfen wird noch nicht unterstützt.",
     "Forwarded message" : "Weitergeleitete Nachricht",
     "Error while loading the selected message." : "Fehler beim Laden der ausgewählten Nachricht.",
-    "Draft saved!" : "Entwurf gespeichert!",
     "Choose a file to add as attachment" : "Wählen Sie eine Datei, die als Anhang angefügt werden soll",
     "Reply" : "Antwort",
     "Send" : "Senden",
@@ -27,11 +27,11 @@ OC.L10N.register(
     "Server error" : "Serverfehler",
     "you" : "Sie",
     "Send message to {email}" : "Nachricht an {email} senden",
-    "Error while deleting message." : "Fehler beim Löschen der Nachricht.",
     "Checking messages" : "Nachrichten überprüfen",
     "Loading …" : "Laden…",
     "Load more …" : "Weitere laden…",
     "Check messages" : "Nachrichten überprüfen",
+    "Error while deleting message." : "Fehler beim Löschen der Nachricht.",
     "Error while deleting account." : "Fehler beim Löschen des Kontos.",
     "Connecting" : "Verbinde",
     "Connect" : "Verbinden",
@@ -52,6 +52,8 @@ OC.L10N.register(
     "New message" : "Neue Nachricht",
     "Forwarding you to %s - click here if you are not automatically redirected within the next few seconds." : "Sie werden zu %s weitergeleitet - Bitte hier klicken, wenn Sie nicht automatisch innerhalb der nächsten Sekunden weitergeleitet werden.",
     "Redirect warning" : "Umleitungswarnung",
-    "If you do not want to visit that page, you can return to <a href=\"%s\">the mail app</a>." : "Wenn Sie diese Seite nicht besuchen möchten, können Sie <a href=\"%s\">zur Mail-App</a> zurück kehren."
+    "The previous page is sending you to %s" : "Die vorangegangene Seite leitet Sie zu %s weiter",
+    "If you do not want to visit that page, you can return to <a href=\"%s\">the mail app</a>." : "Wenn Sie diese Seite nicht besuchen möchten, können Sie <a href=\"%s\">zur Mail-App</a> zurück kehren.",
+    "Continue to %s" : "Fortsetzen auf %s"
 },
 "nplurals=2; plural=(n != 1);");
