9fad69113f8e700193705b29907d7d1be52bc034
Tahaa Karim <tahaalibra@gmail.com>
2016-09-08 11:54:05 +0530

    fixing indentations

--- tests/controller/aliasescontrollertest.php
+++ tests/controller/aliasescontrollertest.php
@@ -27,80 +27,91 @@ use OCP\IUserSession;
 use Test\TestCase;
 
 class AliasesControllerTest extends TestCase {
-    private $controller;
-    private $appName = 'mail';
-    private $request;
-    private $aliasService;
-    private $userId = 'user12345';
-    private $userSession;
-    private $user;
-    private $alias;
-    public function setUp() {
-        $this->request = $this->getMockBuilder('OCP\IRequest')
-            ->getMock();
-        $this->aliasService = $this->getMockBuilder('OCA\Mail\Service\AliasesService')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->userSession = $this->getMockBuilder('OCP\IUserSession')
-            ->getMock();
-        $this->user = $this->getMockBuilder('OCP\IUser')
-            ->getMock();
-        $this->alias = $this->getMockBuilder('\OCA\Mail\Db\Alias')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->userSession->expects($this->once())
-            ->method('getUser')
-            ->will($this->returnValue($this->user));
-        $this->controller = new AliasesController($this->appName, $this->request, $this->aliasService, $this->userSession);
-    }
-    public function testIndex() {
-        $accountId = 123;
-        $this->user->expects($this->once())
-            ->method('getUID')
-            ->will($this->returnValue($this->userId));
-        $this->aliasService->expects($this->once())
-            ->method('findAll')
-            ->with($accountId, $this->userId)
-            ->will($this->returnValue([$this->alias]));
-        $response = $this->controller->index($accountId);
-        $expectedResponse = [
-            $this->alias
-        ];
-        $this->assertEquals($expectedResponse, $response);
-    }
-    public function testDestroy() {
-        $aliasId = 123;
-        $this->user->expects($this->once())
-            ->method('getUID')
-            ->will($this->returnValue($this->userId));
-        $this->aliasService->expects($this->once())
-            ->method('delete')
-            ->with($this->equalTo($aliasId), $this->equalTo($this->userId))
-            ->will($this->returnValue(new JSONResponse()));
-        $response = $this->controller->destroy($aliasId);
-        $expectedResponse = new JSONResponse();
-        $this->assertEquals($expectedResponse, $response);
-    }
-    public function testCreate() {
-        $accountId = 123;
-        $alias = "alias@marvel.com";
-        $aliasName = "Peter Parker";
-        $this->aliasService->expects($this->once())
-            ->method('create')
-            ->with($this->equalTo($accountId), $this->equalTo($alias), $this->equalTo($aliasName))
-            ->will($this->returnValue([
-                'accountId' => $accountId,
-                'name' => $aliasName,
-                'alias' => $alias,
-                'id' => 123
-            ]));
-        $response = $this->controller->create($accountId, $alias, $aliasName);
-        $expected = [
-            'accountId' => $accountId,
-            'name' => $aliasName,
-            'alias' => $alias,
-            'id' => 123
-        ];
-        $this->assertEquals($expected, $response);
-    }
+	private $controller;
+	private $appName = 'mail';
+	private $request;
+	private $aliasService;
+	private $userId = 'user12345';
+	private $userSession;
+	private $user;
+	private $alias;
+
+	public function setUp() {
+		$this->request = $this->getMockBuilder('OCP\IRequest')
+			->getMock();
+		$this->aliasService = $this->getMockBuilder('OCA\Mail\Service\AliasesService')
+			->disableOriginalConstructor()
+			->getMock();
+		$this->userSession = $this->getMockBuilder('OCP\IUserSession')
+			->getMock();
+		$this->user = $this->getMockBuilder('OCP\IUser')
+			->getMock();
+		$this->alias = $this->getMockBuilder('\OCA\Mail\Db\Alias')
+			->disableOriginalConstructor()
+			->getMock();
+		$this->userSession->expects($this->once())
+			->method('getUser')
+			->will($this->returnValue($this->user));
+		
+		$this->controller = new AliasesController($this->appName, $this->request, $this->aliasService, $this->userSession);
+	}
+
+	public function testIndex() {
+		$accountId = 123;
+		$this->user->expects($this->once())
+			->method('getUID')
+			->will($this->returnValue($this->userId));
+		$this->aliasService->expects($this->once())
+			->method('findAll')
+			->with($accountId, $this->userId)
+			->will($this->returnValue([$this->alias]));
+
+		$response = $this->controller->index($accountId);
+
+		$expectedResponse = [
+			$this->alias
+		];
+		$this->assertEquals($expectedResponse, $response);
+	}
+
+	public function testDestroy() {
+		$aliasId = 123;
+		$this->user->expects($this->once())
+			->method('getUID')
+			->will($this->returnValue($this->userId));
+		$this->aliasService->expects($this->once())
+			->method('delete')
+			->with($this->equalTo($aliasId), $this->equalTo($this->userId))
+			->will($this->returnValue(new JSONResponse()));
+
+		$response = $this->controller->destroy($aliasId);
+
+		$expectedResponse = new JSONResponse();
+		$this->assertEquals($expectedResponse, $response);
+	}
+
+	public function testCreate() {
+		$accountId = 123;
+		$alias = "alias@marvel.com";
+		$aliasName = "Peter Parker";
+		$this->aliasService->expects($this->once())
+			->method('create')
+			->with($this->equalTo($accountId), $this->equalTo($alias), $this->equalTo($aliasName))
+			->will($this->returnValue([
+				'accountId' => $accountId,
+				'name' => $aliasName,
+				'alias' => $alias,
+				'id' => 123
+			]));
+
+		$response = $this->controller->create($accountId, $alias, $aliasName);
+
+		$expected = [
+			'accountId' => $accountId,
+			'name' => $aliasName,
+			'alias' => $alias,
+			'id' => 123
+		];
+		$this->assertEquals($expected, $response);
+	}
 }
--- tests/db/aliasmappertest.php
+++ tests/db/aliasmappertest.php
@@ -24,76 +24,80 @@ namespace OCA\Mail\Db;
 use OC\AppFramework\Db\Db;
 
 class AliasMapperTest extends \PHPUnit_Framework_TestCase {
-    /**
-     * @var AliasMapper
-     */
-    private $mapper;
-    /**
-     * @var \OC\DB\Connection
-     */
-    private $db;
-    /**
-     * @var Alias
-     */
-    private $alias;
-    /**
-     * Initialize Mapper
-     */
-    public function setup(){
-        $db = \OC::$server->getDatabaseConnection();
-        $this->db = new Db($db);
-        $this->mapper = new AliasMapper($this->db);
-    }
-    public function testFind(){
-        $accountMapper = new MailAccountMapper($this->db);
-        $account = new MailAccount();
-        $account->setName('Peter Parker');
-        $account->setInboundHost('mail.marvel.com');
-        $account->setInboundPort(159);
-        $account->setInboundUser('spiderman');
-        $account->setInboundPassword('xxxxxxxx');
-        $account->setInboundSslMode('tls');
-        $account->setEmail('peter.parker@marvel.com');
-        $account->setOutboundHost('smtp.marvel.com');
-        $account->setOutboundPort(458);
-        $account->setOutboundUser('spiderman');
-        $account->setOutboundPassword('xxxx');
-        $account->setOutboundSslMode('ssl');
-        $account->setUserId('user12345');
-        $a = $accountMapper->insert($account);
-        $this->alias = new Alias();
-        $this->alias->setAccountId($a->getId());
-        $this->alias->setAlias('alias@marvel.com');
-        $this->alias->setName('alias');
-        /** @var Alias $b */
-        $b = $this->mapper->insert($this->alias);
-        $result = $this->mapper->find($b->getId(), $account->getUserId());
-        $this->assertEquals(
-            [
-                'accountId' => $this->alias->getAccountId(),
-                'name' => $this->alias->getName(),
-                'alias' => $this->alias->getAlias(),
-                'id' => $this->alias->getId()
-            ],
-            [
-                'accountId' => $result->getAccountId(),
-                'name' => $result->getName(),
-                'alias' => $result->getAlias(),
-                'id' => $result->getId()
-            ]
-        );
-    }
-    protected function tearDown() {
-        parent::tearDown();
-        $sql = 'DELETE FROM *PREFIX*mail_aliases WHERE `id` = ?';
-        $stmt = $this->db->prepare($sql);
-        if (!empty($this->alias)) {
-            $stmt->execute([$this->alias->getId()]);
-        }
-        $sql = 'DELETE FROM *PREFIX*mail_accounts WHERE `user_id` = ?';
-        $stmt = $this->db->prepare($sql);
-        if (!empty($this->alias)) {
-            $stmt->execute(['user12345']);
-        }
-    }
+	/**
+	 * @var AliasMapper
+	 */
+	private $mapper;
+	/**
+	 * @var \OC\DB\Connection
+	 */
+	private $db;
+	/**
+	 * @var Alias
+	 */
+	private $alias;
+	/**
+	 * Initialize Mapper
+	 */
+	public function setup(){
+		$db = \OC::$server->getDatabaseConnection();
+		$this->db = new Db($db);
+		$this->mapper = new AliasMapper($this->db);
+	}
+
+	public function testFind(){
+		$accountMapper = new MailAccountMapper($this->db);
+		$account = new MailAccount();
+		$account->setName('Peter Parker');
+		$account->setInboundHost('mail.marvel.com');
+		$account->setInboundPort(159);
+		$account->setInboundUser('spiderman');
+		$account->setInboundPassword('xxxxxxxx');
+		$account->setInboundSslMode('tls');
+		$account->setEmail('peter.parker@marvel.com');
+		$account->setOutboundHost('smtp.marvel.com');
+		$account->setOutboundPort(458);
+		$account->setOutboundUser('spiderman');
+		$account->setOutboundPassword('xxxx');
+		$account->setOutboundSslMode('ssl');
+		$account->setUserId('user12345');
+		$a = $accountMapper->insert($account);
+		$this->alias = new Alias();
+		$this->alias->setAccountId($a->getId());
+		$this->alias->setAlias('alias@marvel.com');
+		$this->alias->setName('alias');
+		/** @var Alias $b */
+		$b = $this->mapper->insert($this->alias);
+
+		$result = $this->mapper->find($b->getId(), $account->getUserId());
+
+		$this->assertEquals(
+			[
+				'accountId' => $this->alias->getAccountId(),
+				'name' => $this->alias->getName(),
+				'alias' => $this->alias->getAlias(),
+				'id' => $this->alias->getId()
+			],
+			[
+				'accountId' => $result->getAccountId(),
+				'name' => $result->getName(),
+				'alias' => $result->getAlias(),
+				'id' => $result->getId()
+			]
+		);
+	}
+	protected function tearDown() {
+		parent::tearDown();
+
+		$sql = 'DELETE FROM *PREFIX*mail_aliases WHERE `id` = ?';
+		$stmt = $this->db->prepare($sql);
+		if (!empty($this->alias)) {
+			$stmt->execute([$this->alias->getId()]);
+		}
+		$sql = 'DELETE FROM *PREFIX*mail_accounts WHERE `user_id` = ?';
+		$stmt = $this->db->prepare($sql);
+		if (!empty($this->alias)) {
+			$stmt->execute(['user12345']);
+		}
+	}
 }
