1a5ee1098d17c63eb281264fac269d2bdfb6ad73
Steffen Lindner <mail@steffen-lindner.de>
2016-09-19 11:00:35 +0200

    Add JS Coverage

--- karma.conf.js
+++ karma.conf.js
@@ -32,11 +32,20 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
+			'js/*.js': ['coverage'],
+			'js/*/*.js': ['coverage']
 		},
+
+		// Configure code coverage reporter
+		coverageReporter: {
+			type : 'lcov',
+			dir : 'coverage/'
+		},
+
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
+		reporters: ['progress', 'coverage'],
 		// web server port
 		port: 9876,
 		// enable / disable colors in the output (reporters and logs)
