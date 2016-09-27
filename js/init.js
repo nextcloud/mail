9225c3064b56e09e82a8733b615a7d2c7abaf7d9
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-09-27 22:49:33 +0200

    add integration test for starting the app

---
+++ js/init.js
@@ -0,0 +1,28 @@
+/**
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * Mail
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+define(function(require) {
+	var App = require('app');
+
+	$(function() {
+		// Start app when the page is ready
+		console.log('starting Mail…');
+		App.start();
+	});
+});
---
+++ js/tests/integration/app_start_spec.js
@@ -0,0 +1,97 @@
+/**
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * Mail
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+define([
+	'app',
+	'cache',
+	'radio',
+	'backbone',
+	'controller/accountcontroller',
+	'models/accountcollection'
+], function(Mail, Cache, Radio, Backbone, AccountController,
+	AccountCollection) {
+	describe('App', function() {
+
+		beforeEach(function() {
+			jasmine.Ajax.install();
+			$('testcontainer').remove();
+			$('body')
+				.append('testcontainer')
+				.append(
+					'<input type="hidden" id="config-installed-version" value="0.6.1">'
+					+ '<input type="hidden" id="serialized-accounts" value="">'
+					+ '<div id="user-displayname">Jane Doe</div>'
+					+ '<div id="user-email">jane@doe.cz</div>'
+					+ '<div id="app">'
+					+ '	<div id="app-navigation" class="icon-loading">'
+					+ '		<div id="mail-new-message-fixed"></div>'
+					+ '		<ul>'
+					+ '			<li id="app-navigation-accounts"></li>'
+					+ '		</ul>'
+					+ '		<div id="app-settings">'
+					+ '			<div id="app-settings-header">'
+					+ '				<button class="settings-button" data-apps-slide-toggle="#app-settings-content"></button>'
+					+ '			</div>'
+					+ '			<div id="app-settings-content"></div>'
+					+ '		</div>'
+					+ '	</div>'
+					+ '	<div id="app-content">'
+					+ '		<div class="mail-content container">'
+					+ '			<div class="container icon-loading"></div>'
+					+ '		</div>'
+					+ '	</div>'
+					+ '</div>');
+		});
+
+		afterEach(function() {
+			jasmine.Ajax.uninstall();
+		});
+
+		it('starts', function() {
+			var accountsDeferred = $.Deferred();
+
+			spyOn(Cache, 'init');
+			spyOn(Radio.ui, 'trigger');
+			spyOn(Backbone.history, 'start');
+			spyOn(AccountController, 'loadAccounts').and.callFake(function() {
+				return accountsDeferred.promise();
+			});
+
+			// No ajax calls so far
+			expect(jasmine.Ajax.requests.count()).toBe(0);
+
+			// Let's go…
+			Mail.start();
+
+			expect(Cache.init).toHaveBeenCalled();
+			expect(Radio.ui.trigger).toHaveBeenCalledWith('content:loading');
+
+			var accounts = new AccountCollection([
+				{
+					accountId: 44,
+					name: 'Jane Doe',
+					email: 'jane@doe.se'
+				}
+			]);
+			accountsDeferred.resolve(accounts);
+
+			expect(Backbone.history.start).toHaveBeenCalled();
+		});
+	});
+});
