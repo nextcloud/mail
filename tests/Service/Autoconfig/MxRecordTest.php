5b61b5048273ea65d4da31f7e4e62ff35774cf54
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-08-07 13:12:20 +0200

    Test MxRecord class
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ tests/Service/Autoconfig/MxRecordTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * Mail
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\Mail\Tests\Service\Autoconfig;
+
+use OCA\Mail\Service\AutoConfig\MxRecord;
+use OCA\Mail\Service\Logger;
+use PHPUnit_Framework_TestCase;
+
+class MxRecordTest extends PHPUnit_Framework_TestCase {
+
+	/** @var MxRecord */
+	private $record;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$logger = $this->createMock(Logger::class);
+		$this->record = new MxRecord($logger);
+	}
+
+	public function testQuery() {
+		$records = $this->record->query('nextcloud.com');
+
+		$this->assertInternalType('array', $records);
+		$this->assertNotEmpty($records);
+	}
+
+	public function testQueryNoRecord() {
+		$records = $this->record->query('example.com');
+
+		$this->assertFalse($records);
+	}
+
+}
