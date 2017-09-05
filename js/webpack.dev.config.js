7c59f07cc43a242ceb5aa69b058bfbeb9672f9aa
Steffen Lindner <mail@steffen-lindner.de>
2017-09-05 08:57:29 +0200

    Quick minify config for webpack

---
+++ js/webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	entry: './js/init.js',
+	output: {
+		filename: 'build.js',
+		path: path.resolve(__dirname, 'build')
+	},
+	resolve: {
+		modules: [path.resolve(__dirname), 'node_modules'],
+		alias: {
+			'handlebars': 'handlebars/runtime.js'
+		}
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{test: /davclient/, use: 'exports-loader?dav'},
+			{test: /\.html$/, loader: "handlebars-loader", query: {
+					extensions: '.html',
+					helperDirs: __dirname + '/templatehelpers'
+				}}
+		],
+		loaders: [
+			{test: /ical/, loader: 'exports-loader?ICAL'}
+		]
+	}
+};
