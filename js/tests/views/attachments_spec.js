88b440c1cbd8a9173b6f8dbb49b3e4f7fd4da4aa
Jan-Christoph Borchardt <hey@jancborchardt.net>
2017-09-28 12:50:49 +0200

    Use Files icon for 'Add from Files' button
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- js/tests/views/attachments_spec.js
+++ js/tests/views/attachments_spec.js
@@ -38,7 +38,7 @@ define([
   <span class="icon-upload"></span> Add attachment\n\
 </button>\n\
 <button type="button" id="add-cloud-attachment" style="display: inline-block;">\n\
-  <span class="icon-edit"></span> Add from Files\n\
+  <span class="icon-folder"></span> Add from Files\n\
 </button>\n\
 <input type="file" multiple="" id="local-attachments" style="display: none;">\n');
 		});
