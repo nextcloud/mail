01b66029944e94b310cfb5f714702d3d5a97034e
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-11-13 18:46:08 +0100

    Add proper sourcemap support, esp. for unit test failures
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- karma.conf.js
+++ karma.conf.js
@@ -33,10 +33,10 @@ module.exports = function (config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'js/**[!vendor]/*[!spec].js': ['coverage'],
+			'js/**[!vendor]/*[!spec].js': ['coverage', 'sourcemap'],
 			// add webpack as preprocessor
-			'js/tests/*_spec.js': ['webpack'],
-			'js/tests/**/*_spec.js': ['webpack']
+			'js/tests/*_spec.js': ['webpack', 'sourcemap'],
+			'js/tests/**/*_spec.js': ['webpack', 'sourcemap']
 		},
 
 		webpackMiddleware: {
