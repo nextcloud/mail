a97797e98b030d438300fce238e4678c200e4b05
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-20 14:14:14 +0200

    Fix 'home' -> 'folder' redirection
    This should only happen if the current route is 'home', otherwise it
    always redirects to the first account's first folder on reload.
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- js/views/Home.vue
+++ js/views/Home.vue
@@ -43,7 +43,7 @@
 
 				console.debug('accounts fetched', accounts)
 
-				if (accounts.length > 0) {
+				if (this.$route.name === 'home' && accounts.length > 0) {
 					// Show first account
 
 					let firstAccount = accounts[0]
