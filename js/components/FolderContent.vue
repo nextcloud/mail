a84be4fd1d602fc3b7715657ad64746b7cebea0b
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-20 13:23:44 +0200

    Show message details only if there are messages
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- js/components/FolderContent.vue
+++ js/components/FolderContent.vue
@@ -6,7 +6,7 @@
 			<template v-else>
 				<MessageList/>
 				<NewMessageDetail v-if="newMessage"/>
-				<Message v-else-if="message"/>
+				<Message v-else-if="hasMessages"/>
 			</template>
 		</div>
 	</div>
@@ -30,33 +30,51 @@
 			return {
 				loading: true,
 				newMessage: false,
-				message: false,
+			}
+		},
+		computed: {
+			hasMessages () {
+				return this.$store.getters.getEnvelopes(
+					this.$route.params.accountId,
+					this.$route.params.folderId
+				).length > 0
 			}
 		},
 		created () {
-			this.$store.dispatch(
-				'fetchEnvelopes', {
-					accountId: this.$route.params.accountId,
-					folderId: this.$route.params.folderId
-				}).then(envelopes => {
-				this.loading = false;
-
-				if (envelopes.length > 0) {
-					// Show first message
-					let first = envelopes[0];
-
-					this.message = true;
-
-					this.$router.replace({
-						name: 'message',
-						params: {
-							accountId: this.$route.params.accountId,
-							folderId: this.$route.params.folderId,
-							messageId: first.id,
-						}
-					})
+			this.fetchData()
+		},
+		watch: {
+			'$route' (to, from) {
+				if (to.name === 'folder') {
+					// Navigate (back) to the folder view -> (re)fetch data
+					this.fetchData()
 				}
-			});
+			}
+		},
+		methods: {
+			fetchData () {
+				this.$store.dispatch(
+					'fetchEnvelopes', {
+						accountId: this.$route.params.accountId,
+						folderId: this.$route.params.folderId
+					}).then(envelopes => {
+					this.loading = false;
+
+					if (envelopes.length > 0) {
+						// Show first message
+						let first = envelopes[0];
+
+						this.$router.replace({
+							name: 'message',
+							params: {
+								accountId: this.$route.params.accountId,
+								folderId: this.$route.params.folderId,
+								messageId: first.id,
+							}
+						})
+					}
+				});
+			}
 		}
 	}
 </script>
