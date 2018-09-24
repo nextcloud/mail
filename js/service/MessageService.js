1b906b18122a145c6ad331270b4b0185597cfa4c
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-24 08:16:38 +0200

    Load real message body data from the server
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- js/service/MessageService.js
+++ js/service/MessageService.js
@@ -10,37 +10,11 @@ export function fetchEnvelopes (accountId, folderId) {
 }
 
 export function fetchMessage (accountId, folderId, id) {
-	return new Promise((res, rej) => {
-		setTimeout(() => {
-			res({
-				id: id,
-				from: [
-					{
-						label: 'Backbone Marionette',
-						email: 'backbone.marionette@frameworks.js',
-					}
-				],
-				to: [
-					{
-						label: 'React',
-						email: 'react@frameworks.js',
-					},
-					{
-						label: 'Angular',
-						email: 'angular@frameworks.js',
-					}
-				],
-				cc: [
-					{
-						label: 'Underscore Jayes',
-						email: 'underscore@frameworks.js',
-					}
-				],
-				subject: 'Do you enjoy the Vue?',
-				hasHtmlBody: false,
-				body: 'Henlo!',
-				signature: 'Backbone Marionette',
-			})
-		}, 1500)
+	const url = OC.generateUrl('/apps/mail/api/accounts/{accountId}/folders/{folderId}/messages/{id}', {
+		accountId,
+		folderId,
+		id
 	})
+
+	return HttpClient.get(url).then(resp => resp.data)
 }
