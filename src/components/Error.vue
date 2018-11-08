ba6d981dc98e0160f56bd2c5fae46659e5b2ce1e
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-11-08 12:35:58 +0100

    Add Error component
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/components/Error.vue
@@ -0,0 +1,47 @@
+<!--
+  - @copyright 2018 Christoph Wurst <christoph@winzerhof-wurst.at>
+  -
+  - @author 2018 Christoph Wurst <christoph@winzerhof-wurst.at>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+	<div class="emptycontent">
+		<h2>{{ error }}</h2>
+		<p>{{ message }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Error',
+		props: {
+			error: {
+				type: String,
+				required: true,
+			},
+			message: {
+				type: String,
+				required: true,
+			},
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
