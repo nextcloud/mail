3410205cfe3243f8cb7d24b0835f26a976538016
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-03-25 15:05:02 +0100

    Dispatch an interaction event for sent messages
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ lib/Listener/InteractionListener.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2020 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @author 2020 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\Mail\Listener;
+
+use OCA\Mail\Address;
+use OCA\Mail\Events\MessageSentEvent;
+use OCP\Contacts\Events\ContactInteractedWithEvent;
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\EventDispatcher\IEventListener;
+use OCP\ILogger;
+use OCP\IUserSession;
+
+class InteractionListener implements IEventListener {
+
+	/** @var IEventDispatcher */
+	private $dispatcher;
+
+	/** @var IUserSession */
+	private $userSession;
+
+	/** @var ILogger */
+	private $logger;
+
+	public function __construct(IEventDispatcher $dispatcher,
+								IUserSession $userSession,
+								ILogger $logger) {
+		$this->dispatcher = $dispatcher;
+		$this->userSession = $userSession;
+		$this->logger = $logger;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function handle(Event $event): void {
+		if (!($event instanceof MessageSentEvent)) {
+			return;
+		}
+
+		$user = $this->userSession->getUser();
+		$recipients = $event->getMessage()->getTo()
+			->merge($event->getMessage()->getCC())
+			->merge($event->getMessage()->getBCC());
+		foreach ($recipients->iterate() as $recipient) {
+			/** @var Address $recipient */
+			$interactionEvent = new ContactInteractedWithEvent($user);
+			$email = $recipient->getEmail();
+			if ($email === null) {
+				// Weird, bot ok
+				continue;
+			}
+			$interactionEvent->setEmail($email);
+			$this->dispatcher->dispatch(ContactInteractedWithEvent::class, $interactionEvent);
+		}
+	}
+
+}
