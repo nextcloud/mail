380526dc8e6fa936fd09b08a72ac9d76dc946a85
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-04-20 16:39:24 +0200

    Limit the size of recipient labels
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Db/MessageMapper.php
+++ lib/Db/MessageMapper.php
@@ -36,6 +36,7 @@ use OCP\IDBConnection;
 use function array_combine;
 use function array_keys;
 use function array_map;
+use function mb_substr;
 
 class MessageMapper extends QBMapper {
 
@@ -146,7 +147,7 @@ class MessageMapper extends QBMapper {
 
 					$qb2->setParameter('message_id', $messageId, IQueryBuilder::PARAM_INT);
 					$qb2->setParameter('type', $type, IQueryBuilder::PARAM_INT);
-					$qb2->setParameter('label', $recipient->getLabel(), IQueryBuilder::PARAM_STR);
+					$qb2->setParameter('label', mb_substr($recipient->getLabel(), 0, 255), IQueryBuilder::PARAM_STR);
 					$qb2->setParameter('email', $recipient->getEmail(), IQueryBuilder::PARAM_STR);
 
 					$qb2->execute();
