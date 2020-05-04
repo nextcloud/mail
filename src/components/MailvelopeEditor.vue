c193e3168cbec2ad2a2a2945de6d5b61e33a7ed1
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-05-04 17:28:12 +0200

    Fix mailvelope integration by switching to the API version
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/components/MailvelopeEditor.vue
@@ -0,0 +1,82 @@
+<!--
+  - @copyright 2020 Christoph Wurst <christoph@winzerhof-wurst.at>
+  -
+  - @author 2020 Christoph Wurst <christoph@winzerhof-wurst.at>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+	<div id="mailvelope-composer"></div>
+</template>
+
+<script>
+import logger from '../logger'
+import {isPgpgMessage} from '../crypto/pgp'
+
+export default {
+	name: 'MailvelopeEditor',
+	props: {
+		value: {
+			type: String,
+			required: true,
+		},
+		recipients: {
+			type: Array,
+			required: true,
+		},
+		quotedText: {
+			type: Object,
+			required: false,
+			default: () => undefined,
+		},
+		isReplyOrForward: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data() {
+		return {
+			editor: undefined,
+		}
+	},
+	async mounted() {
+		const isEncrypted = this.quotedText ? isPgpgMessage(this.quotedText) : false
+		const quotedMail = this.isReplyOrForward ? this.quotedText?.value : undefined
+
+		this.editor = await window.mailvelope.createEditorContainer('#mailvelope-composer', undefined, {
+			quotedMail: isEncrypted ? quotedMail : undefined,
+		})
+	},
+	methods: {
+		async pull() {
+			const recipients = this.recipients.map((r) => r.email)
+			logger.info('encrypting message', {recipients})
+			const armored = await this.editor.encrypt(recipients)
+			logger.info('message encryted', {armored})
+
+			this.$emit('input', armored)
+		},
+	},
+}
+</script>
+
+<style scoped>
+#mailvelope-composer {
+	width: 100%;
+	height: 450px;
+}
+</style>
---
+++ src/tests/unit/crypto/mailvelope.spec.js
@@ -0,0 +1,49 @@
+/*
+ * @copyright 2020 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @author 2020 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {getMailvelope} from '../../../crypto/mailvelope'
+
+describe('mailvelope', () => {
+	afterEach(() => {
+		delete window.mailvelope
+	})
+
+	it('loads statically', async () => {
+		window.mailvelope = {
+			mock: 3,
+		}
+
+		const mailvelope = await getMailvelope()
+
+		expect(mailvelope).to.deep.equal(window.mailvelope)
+	})
+
+	it('loads dynamically', async () => {
+		const p = getMailvelope()
+		window.mailvelope = {
+			mock: 3,
+		}
+		window.dispatchEvent(new Event('mailvelope'))
+
+		const mailvelope = await p
+		expect(mailvelope).to.deep.equal(window.mailvelope)
+	})
+})
