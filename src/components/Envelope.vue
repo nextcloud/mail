1072c67f0677ac28b51736f5f133fc3413f819e9
Cyrille Bollu <cyr.debian@bollu.be>
2020-11-03 18:17:44 +0100

    Fix to https://github.com/nextcloud/mail/issues/3956
    Is it me or did the 'read flag' got renamed from 'unseen' to 'seen'?
    
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>
    Co-Authored-By: Christoph Wurst <christoph@winzerhof-wurst.at>
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/components/Envelope.vue
+++ src/components/Envelope.vue
@@ -236,7 +236,7 @@ export default {
 			this.$store.dispatch('toggleEnvelopeImportant', this.data)
 		},
 		onToggleSeen() {
-			this.$store.dispatch('toggleEnvelopeSeen', this.data)
+			this.$store.dispatch('toggleEnvelopeSeen', { envelope: this.data })
 		},
 		onToggleJunk() {
 			this.$store.dispatch('toggleEnvelopeJunk', this.data)
