364cce2721bc6aebb51259cefa1534bd8cc37da5
Richard Steinmetz <richard@steinmetz.cloud>
2020-11-23 16:33:02 +0100

    Fix previous drafts not being deleted from the cache
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- tests/Unit/Listener/DeleteDraftListenerTest.php
+++ tests/Unit/Listener/DeleteDraftListenerTest.php
@@ -31,6 +31,7 @@ use OCA\Mail\Db\Mailbox;
 use OCA\Mail\Db\MailboxMapper;
 use OCA\Mail\Db\Message;
 use OCA\Mail\Events\DraftSavedEvent;
+use OCA\Mail\Events\MessageDeletedEvent;
 use OCA\Mail\Events\MessageSentEvent;
 use OCA\Mail\IMAP\IMAPClientFactory;
 use OCA\Mail\IMAP\MailboxSync;
@@ -41,6 +42,7 @@ use OCA\Mail\Model\NewMessageData;
 use OCA\Mail\Model\RepliedMessageData;
 use OCP\AppFramework\Db\DoesNotExistException;
 use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventDispatcher;
 use OCP\EventDispatcher\IEventListener;
 use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Log\LoggerInterface;
@@ -62,6 +64,9 @@ class DeleteDraftListenerTest extends TestCase {
 	/** @var LoggerInterface|MockObject */
 	private $logger;
 
+	/** @var IEventDispatcher|MockObject */
+	private $eventDispatcher;
+
 	/** @var IEventListener */
 	private $listener;
 
@@ -73,13 +78,15 @@ class DeleteDraftListenerTest extends TestCase {
 		$this->messageMapper = $this->createMock(MessageMapper::class);
 		$this->mailboxSync = $this->createMock(MailboxSync::class);
 		$this->logger = $this->createMock(LoggerInterface::class);
+		$this->eventDispatcher = $this->createMock(IEventDispatcher::class);
 
 		$this->listener = new DeleteDraftListener(
 			$this->imapClientFactory,
 			$this->mailboxMapper,
 			$this->messageMapper,
 			$this->mailboxSync,
-			$this->logger
+			$this->logger,
+			$this->eventDispatcher
 		);
 	}
 
@@ -105,6 +112,8 @@ class DeleteDraftListenerTest extends TestCase {
 			->method('addFlag');
 		$this->logger->expects($this->never())
 			->method('error');
+		$this->eventDispatcher->expects($this->never())
+			->method('dispatchTyped');
 
 		$this->listener->handle($event);
 	}
@@ -182,6 +191,8 @@ class DeleteDraftListenerTest extends TestCase {
 			->method('addFlag');
 		$this->logger->expects($this->never())
 			->method('error');
+		$this->eventDispatcher->expects($this->never())
+			->method('dispatchTyped');
 
 		$this->listener->handle($event);
 	}
@@ -233,6 +244,15 @@ class DeleteDraftListenerTest extends TestCase {
 			->with('Drafts');
 		$this->logger->expects($this->never())
 			->method('error');
+
+		$messageDeletedEvent = new MessageDeletedEvent(
+			$account,
+			$mailbox,
+			$draft->getUid()
+		);
+		$this->eventDispatcher->expects($this->once())
+			->method('dispatchTyped')
+			->with($messageDeletedEvent);
 
 		$this->listener->handle($event);
 	}
