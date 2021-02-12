efbd862c88935a4c11f40faf211e88b081fea6e5
Richard Steinmetz <richard@steinmetz.cloud>
2021-02-12 15:24:38 +0100

    Remove obsolete iframe css injection code
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/html-response.js
+++ src/html-response.js
@@ -24,15 +24,3 @@ import '../css/html-response.css'
 
 // iframe-resizer client script
 import 'iframe-resizer/js/iframeResizer.contentWindow.js'
-window.iFrameResizer = {
-	onMessage: (message) => {
-		if (!message.cssVars) {
-			return
-		}
-
-		// inject received css vars
-		Object.entries(message.cssVars).forEach(([key, val]) => {
-			document.documentElement.style.setProperty(key, val)
-		})
-	},
-}
