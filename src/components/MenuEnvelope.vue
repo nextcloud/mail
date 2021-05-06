f72d5f0f4e86e3fded73a4ea716c1affbb60e312
Johannes Brückner <johannes@dotplex.com>
2021-05-06 08:14:26 +0200

    View source override

--- src/components/MenuEnvelope.vue
+++ src/components/MenuEnvelope.vue
@@ -89,7 +89,7 @@
 				:disabled="sourceLoading"
 				:close-after-click="true"
 				@click.prevent="onShowSourceModal">
-				{{ t('mail', 'View source') }}
+				{{ dotplexOverrideTranslation(t('mail', 'View source')) }}
 			</ActionButton>
 			<ActionLink v-if="debug"
 				icon="icon-download"
@@ -350,6 +350,14 @@ export default {
 		},
 		onCloseTagModal() {
 			this.showTagModal = false
+		},
+		dotplexOverrideTranslation(string) {
+			switch (string) {
+				case 'Quelle ansehen':
+					return 'Quelltext anzeigen'
+				default:
+					return string
+			}
 		},
 	},
 }
