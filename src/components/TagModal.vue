279af7edd4343d686704dc0cb555c60a15f44803
GretaD <gretadoci@gmail.com>
2021-05-21 15:28:39 +0200

    Some changes
    Signed-off-by: GretaD <gretadoci@gmail.com>

--- src/components/TagModal.vue
+++ src/components/TagModal.vue
@@ -9,48 +9,14 @@
 					style="background-color: rgb(255, 122, 102); color: rgb(0, 0, 0);"
 					@click.prevent="onToggleImportant">
 					{{
-						t('mail', 'Important')
-					}}
-					<button class="removebutton"
-						@click="removeTag">
-						{{ t('mail','Remove') }}
-					</button>
-				</div>
-
-				<div class="tagtodo button"
-					style="background-color: rgb(49, 124, 204); color: rgb(255, 255, 255);"
-					@click.prevent="onToggleImportant">
-					{{
-						t('mail', 'Work')
-					}}
-					<button class="removebutton"
-						@click="removeTag">
-						{{ t('mail','Remove') }}
-					</button>
-				</div>
-				<div class="taglater button"
-					style="background-color: rgb(241, 219, 80); color: rgb(0, 0, 0);"
-					@click.prevent="onToggleImportant">
-					{{
-						t('mail', 'Personal')
-					}}
-					<button class="removebutton"
-						@click="removeTag">
-						{{ t('mail','Remove') }}
-					</button>
-				</div>
-				<div class="tagfinished button"
-					style="background-color: rgb(49, 204, 124); color: rgb(255, 255, 255);"
-					@click.prevent="onToggleImportant">
-					{{
 						t('mail', 'To do')
 					}}
 					<button class="removebutton"
-						@click="removeTag">
+						@click.prevent.stop="removeTag">
 						{{ t('mail','Remove') }}
 					</button>
 				</div>
-			</div>
+		</div>
 		</div>
 	</Modal>
 </template>
@@ -71,17 +37,15 @@ export default {
 			required: true,
 		},
 	},
-	data() {
-		return {
-			color: randomColor(),
-		}
-	},
 	methods: {
 		onClose() {
 			this.$emit('close')
 		},
 		onToggleImportant() {
-			this.$store.dispatch('toggleTagImportant', { envelope: this.envelope })
+			this.$store.dispatch('addEnvelopeTag', { envelope: this.envelope, imapLabel: this.imapLabel })
+		},
+		removeTag() {
+			this.$store.dispatch('removeEnvelopeTag', { envelope: this.envelope, tag: this.tag })
 		},
 	},
 }
