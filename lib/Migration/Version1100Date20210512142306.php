294849332cc75c8e1763eb03857fb3e506d37611
Anna Larch <anna@nextcloud.com>
2021-07-06 14:03:25 +0200

    Add method exists check to migration
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- lib/Migration/Version1100Date20210512142306.php
+++ lib/Migration/Version1100Date20210512142306.php
@@ -31,6 +31,11 @@ class Version1100Date20210512142306 extends SimpleMigrationStep {
 	 * @param array $options
 	 */
 	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+		if (!method_exists($this->mailboxMapper, 'findAllIds')) {
+			$output->warning('New Mail code hasn\'t been loaded yet, skipping tag migration. Please run `occ mail:tags:migration-jobs` after the upgrade.');
+			return;
+		}
+
 		foreach ($this->mailboxMapper->findAllIds() as $mailboxId) {
 			$this->jobList->add(MigrateImportantJob::class, ['mailboxId' => $mailboxId]);
 		}
