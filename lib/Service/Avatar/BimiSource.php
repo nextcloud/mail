451362b39b7e9a0e6a58acc9b793cf18ddcc175c
MeiKatz <gregor.mitzka@gmail.com>
2021-12-06 17:27:19 +0100

    fix code-style
    Signed-off-by: MeiKatz <gregor.mitzka@gmail.com>

--- lib/Service/Avatar/BimiSource.php
+++ lib/Service/Avatar/BimiSource.php
@@ -31,133 +31,147 @@ use OCP\Http\Client\IClientService;
 class BimiSource implements IAvatarSource {
 
   /** @var IClientService */
-  private $clientService;
+	private $clientService;
 
-  /** @var IMimeTypeDetector */
-  private $mimeDetector;
+	/** @var IMimeTypeDetector */
+	private $mimeDetector;
 
-  public function __construct(
-    IClientService $clientService,
-    IMimeTypeDetector $mimeDetector
+	public function __construct(
+	IClientService $clientService,
+	IMimeTypeDetector $mimeDetector
   ) {
-    $this->clientService = $clientService;
-    $this->mimeDetector = $mimeDetector;
-  }
+		$this->clientService = $clientService;
+		$this->mimeDetector = $mimeDetector;
+	}
 
-  /**
-   * Does this source query external services?
-   *
-   * @return bool
-   */
-  public function isExternal(): bool {
-    return true;
-  }
+	/**
+	 * Does this source query external services?
+	 *
+	 * @return bool
+	 */
+	public function isExternal(): bool {
+		return true;
+	}
 
-  /**
-   * @param string $email_address sender email address
-   * @param AvatarFactory $factory
-   * @return Avatar|null avatar URL if one can be found
-   */
-  public function fetch(
-    string $email_address,
-    AvatarFactory $factory
+	/**
+	 * @param string $email_address sender email address
+	 * @param AvatarFactory $factory
+	 * @return Avatar|null avatar URL if one can be found
+	 */
+	public function fetch(
+	string $email_address,
+	AvatarFactory $factory
   ): ?Avatar {
-    $horde = new Horde_Mail_Rfc822_Address( $email_address );
-    // TODO: add support for other selectors
-    $domain = 'default._bimi.' . $horde->host;
+		$horde = new Horde_Mail_Rfc822_Address($email_address);
+		// TODO: add support for other selectors
+		$domain = 'default._bimi.' . $horde->host;
 
-    $iconUrl = $this->getIconUrl( $domain );
+		$iconUrl = $this->getIconUrl($domain);
 
-    if ( is_null( $iconUrl ) ) {
-      return null;
-    }
+		if (is_null($iconUrl)) {
+			return null;
+		}
 
-    /** @var string $iconUrl */
-    $client = $this->clientService->newClient();
+		/** @var string $iconUrl */
+		$client = $this->clientService->newClient();
 
-    try {
-      $response = $client->get( $iconUrl );
-    } catch ( Exception $exception ) {
-      return null;
-    }
+		try {
+			$response = $client->get($iconUrl);
+		} catch (Exception $exception) {
+			return null;
+		}
 
-    // Don't save 0 byte images
-    $body = $response->getBody();
+		// Don't save 0 byte images
+		$body = $response->getBody();
 
-    if ( strlen( $body ) === 0 ) {
-      return null;
-    }
+		if (strlen($body) === 0) {
+			return null;
+		}
 
-    $mime = $this->mimeDetector->detectString( $body );
+		$mime = $this->mimeDetector->detectString($body);
 
-    return $factory->createExternal(
-      $iconUrl,
-      $mime
-    );
-  }
+		return $factory->createExternal(
+	  $iconUrl,
+	  $mime
+	);
+	}
 
-  /**
-   * @param string $domain domain of the BIMI record
-   * @return string|null BIMI record if found
-   */
-  private function getBimiRecord( $domain ): ?string {
-    $records = dns_get_record(
-      $domain,
-      DNS_TXT
-    );
+	/**
+	 * @param string $domain domain of the BIMI record
+	 * @return string|null BIMI record if found
+	 */
+	private function getBimiRecord($domain): ?string {
+		$records = dns_get_record(
+	  $domain,
+	  DNS_TXT
+	);
 
-    foreach ( $records as $record ) {
-      if ( strpos( $record['txt'], 'v=BIMI1;' ) === 0 ) {
-        return $record['txt'];
-      }
-    }
+		foreach ($records as $record) {
+			if (strpos($record['txt'], 'v=BIMI1;') === 0) {
+				return $record['txt'];
+			}
+		}
 
-    return null;
-  }
+		return null;
+	}
 
-  /**
-   * @param string $string
-   * @return array
-   */
-  private function getDnsTags( $string ): array {
-    $fragments = explode( ';', $string );
-    $tags = [];
+	/**
+	 * @param string $string
+	 * @return array
+	 */
+	private function getDnsTags($string): array {
+		$fragments = explode(';', $string);
+		$tags = [];
 
-    foreach ( $fragments as $fragment ) {
-      $pair = explode( '=', $fragment, 2 );
+		foreach ($fragments as $fragment) {
+			$pair = explode('=', $fragment, 2);
 
-      if ( count( $pair ) === 1 ) {
-        continue;
-      }
+			if (count($pair) === 1) {
+				continue;
+			}
 
-      $name = trim( $pair[0] );
-      $value = trim( $pair[1] );
+			$name = trim($pair[0]);
+			$value = trim($pair[1]);
 
-      if ( !empty( $value ) ) {
-        $tags[ $name ] = trim( $value );
-      }
-    }
+			if (!empty($value)) {
+				$tags[ $name ] = trim($value);
+			}
+		}
 
-    return $tags;
-  }
+		return $tags;
+	}
 
-  /**
-   * @param string $domain domain of the BIMI record
-   * @return string|null URL to the brand logo if found
-   */
-  private function getIconUrl( $domain ): ?string {
-    $dns_record = $this->getBimiRecord( $domain );
+	/**
+	 * @param string $domain domain of the BIMI record
+	 * @return string|null URL to the brand logo if found
+	 */
+	private function getIconUrl($domain): ?string {
+		$dns_record = $this->getBimiRecord($domain);
 
-    if ( is_null( $dns_record ) ) {
-      return null;
-    }
+		if (is_null($dns_record)) {
+			return null;
+		}
 
-    $tags = $this->getDnsTags( $dns_record );
+		$tags = $this->getDnsTags($dns_record);
 
-    if ( empty( $tags ) || !isset( $tags['l'] ) ) {
-      return null;
-    }
+		if (empty($tags) || !isset($tags['l'])) {
+			return null;
+		}
 
-    return $tags['l'];
-  }
+		return $tags['l'];
+	}
+
+	/**
+	 * @param string $mime_type MIME type
+	 * @return boolean true if SVG, otherwise false
+	 */
+	private function isSvg($mime_type): bool {
+		return in_array(
+	  $mime_type,
+	  array(
+	  	'image/svg+xml',
+	  	'text/plain',
+	  )
+	);
+	}
 }
