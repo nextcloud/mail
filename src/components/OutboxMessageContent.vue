17b7b6042fe23d59e63e661ce17c8a6f9a45a881
greta <gretadoci@gmail.com>
2022-05-05 12:35:46 +0200

    Add text on 3rd outbox column
    Signed-off-by: greta <gretadoci@gmail.com>

---
+++ src/components/OutboxMessageContent.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="emptycontent">
+		<div class="icon-mail" />
+		<h2>{{ t('mail', 'Pending or not sent messages will show up here') }}</h2>
+		<div />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'OutboxMessageContent',
+	components: {
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
