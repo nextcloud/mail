13896384e2ffd86600f2ce984036f8d660d72b8f
greta <gretadoci@gmail.com>
2022-05-27 09:45:38 +0200

    Fix body composer when reply/forward
    Signed-off-by: greta <gretadoci@gmail.com>

--- src/components/Composer.vue
+++ src/components/Composer.vue
@@ -748,8 +748,10 @@ export default {
 	},
 	async beforeMount() {
 		this.setAlias()
-		this.initBody()
-
+		// there's a race condition on the ckeditor initialization and we need to delay it for the initBody to work
+		this.$nextTick(() => {
+			this.initBody()
+		})
 		await this.onMailvelopeLoaded(await getMailvelope())
 	},
 	mounted() {
