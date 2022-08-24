dd394c5988546c62588a4dffd4e6601011cc1aed
greta <gretadoci@gmail.com>
2022-08-24 18:53:46 +0200

    Fix active state on envelope
    Signed-off-by: greta <gretadoci@gmail.com>

--- src/components/Envelope.vue
+++ src/components/Envelope.vue
@@ -12,6 +12,7 @@
 		:to="link"
 		:data-envelope-id="data.databaseId"
 		:title="addresses"
+		:active="isActive"
 		:details="formatted()"
 		@click="onClick">
 		<template #icon>
@@ -460,6 +461,9 @@ export default {
 				id: this.data.databaseId,
 			})
 		},
+		isActive() {
+			return parseInt(this.$route.params.threadId) === this.data.databaseId
+		},
 	},
 	methods: {
 		setSelected(value) {
@@ -646,10 +650,6 @@ export default {
 }
 list-item-style.draft .app-content-list-item-line-two {
 	font-style: italic;
-}
-.list-item-style.active {
-	background-color: var(--color-primary-light);
-	border-radius: 16px;
 }
 
 .icon-reply,
