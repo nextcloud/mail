e86cc894aa3cfa8e1cbbb6451fe87e7e90f86c99
greta <gretadoci@gmail.com>
2022-11-24 10:57:39 +0100

    Fix scrolling for long threads on narrow screens
    Signed-off-by: greta <gretadoci@gmail.com>

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -272,6 +272,9 @@ export default {
 <style lang="scss">
 #mail-message {
 	margin-bottom: 30vh;
+	max-height: calc(100vh - 50px);
+	overflow: auto;
+	flex-grow: 1;
 
 	.icon-loading {
 		&:only-child:after {
