63d7187ea29f829e0ffc963d640200233d20fc6e
greta <gretadoci@gmail.com>
2022-11-24 17:14:42 +0100

    Fix scrolling regressions for long threads
    Signed-off-by: greta <gretadoci@gmail.com>

--- src/components/MailboxThread.vue
+++ src/components/MailboxThread.vue
@@ -296,7 +296,12 @@ export default {
 	}
 }
 :deep(.button-vue--vue-secondary) {
-	box-shadow: none;
+	position: sticky;
+	top:40px;
+	left: 10px;
+}
+:deep(.app-content-wrapper) {
+	overflow: auto;
 }
 .envelope-list {
 	overflow-y: auto;
