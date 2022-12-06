118671f4d5d85f794f413592509cb7b23615777c
Daniel Kesselberg <mail@danielkesselberg.de>
2022-12-06 23:03:07 +0100

    feat: compatibility layer for legacy exceptions
    Client- and ServiceException is the bridge from Horde to Mail.
    
    Some exceptions from Horde may return a non-numeric exception code.
    To avoid type errors the exception code is converted when non-numeric.
    
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/Exception/ServiceException.php
+++ lib/Exception/ServiceException.php
@@ -24,6 +24,18 @@ declare(strict_types=1);
 namespace OCA\Mail\Exception;
 
 use Exception;
+use Throwable;
 
 class ServiceException extends Exception {
+	/**
+	 * @param string $message [optional] The Exception message to throw.
+	 * @param mixed $code [optional] The Exception code.
+	 * @param null|Throwable $previous [optional] The previous throwable used for the exception chaining.
+	 */
+	public function __construct($message = "", $code = 0, Throwable $previous = null) {
+		if (!is_int($code)) {
+			$code = (int)$code;
+		}
+		parent::__construct($message, $code, $previous);
+	}
 }
