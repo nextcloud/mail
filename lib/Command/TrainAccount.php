056f67f5bffa437adb604811e474f1e77d88300b
Richard Steinmetz <richard@steinmetz.cloud>
2023-01-18 15:03:16 +0100

    fixup! Classify emails based on subjects

--- lib/Command/TrainAccount.php
+++ lib/Command/TrainAccount.php
@@ -11,9 +11,12 @@ namespace OCA\Mail\Command;
 
 use OCA\Mail\Service\AccountService;
 use OCA\Mail\Service\Classification\ClassificationSettingsService;
+use OCA\Mail\Service\Classification\FeatureExtraction\NewCompositeExtractor;
+use OCA\Mail\Service\Classification\FeatureExtraction\VanillaCompositeExtractor;
 use OCA\Mail\Service\Classification\ImportanceClassifier;
 use OCA\Mail\Support\ConsoleLoggerDecorator;
 use OCP\AppFramework\Db\DoesNotExistException;
+use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -23,21 +26,26 @@ use function memory_get_peak_usage;
 
 class TrainAccount extends Command {
 	public const ARGUMENT_ACCOUNT_ID = 'account-id';
+	public const ARGUMENT_NEW = 'new';
+	public const ARGUMENT_SHUFFLE = 'shuffle';
 
 	private AccountService $accountService;
 	private ImportanceClassifier $classifier;
 	private LoggerInterface $logger;
+	private ContainerInterface $container;
 	private ClassificationSettingsService $classificationSettingsService;
 
 	public function __construct(AccountService $service,
 		ImportanceClassifier $classifier,
 		ClassificationSettingsService $classificationSettingsService,
-		LoggerInterface $logger) {
+		LoggerInterface $logger,
+		ContainerInterface $container) {
 		parent::__construct();
 
 		$this->accountService = $service;
 		$this->classifier = $classifier;
 		$this->logger = $logger;
+		$this->container = $container;
 		$this->classificationSettingsService = $classificationSettingsService;
 	}
 
@@ -48,6 +56,8 @@ class TrainAccount extends Command {
 		$this->setName('mail:account:train');
 		$this->setDescription('Train the classifier of new messages');
 		$this->addArgument(self::ARGUMENT_ACCOUNT_ID, InputArgument::REQUIRED);
+		$this->addOption(self::ARGUMENT_NEW, null, null, 'Enable new composite extractor using text based features');
+		$this->addOption(self::ARGUMENT_SHUFFLE, null, null, 'Shuffle data set before training');
 	}
 
 	/**
@@ -62,9 +72,18 @@ class TrainAccount extends Command {
 			$output->writeln("<error>account $accountId does not exist</error>");
 			return 1;
 		}
+
+		/*
 		if (!$this->classificationSettingsService->isClassificationEnabled($account->getUserId())) {
 			$output->writeln("<info>classification is turned off for account $accountId</info>");
 			return 2;
+		}
+		*/
+
+		if ($input->getOption(self::ARGUMENT_NEW)) {
+			$extractor = $this->container->get(NewCompositeExtractor::class);
+		} else {
+			$extractor = $this->container->get(VanillaCompositeExtractor::class);
 		}
 
 		$consoleLogger = new ConsoleLoggerDecorator(
@@ -73,7 +92,9 @@ class TrainAccount extends Command {
 		);
 		$this->classifier->train(
 			$account,
-			$consoleLogger
+			$consoleLogger,
+			$extractor,
+			(bool)$input->getOption(self::ARGUMENT_SHUFFLE),
 		);
 
 		$mbs = (int)(memory_get_peak_usage() / 1024 / 1024);
