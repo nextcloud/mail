a4f98050e989cff6ab2484a4f96fed0611a2982c
Richard Steinmetz <richard@steinmetz.cloud>
2023-01-18 15:03:16 +0100

    fixup! Classify emails based on subjects

---
+++ lib/Service/Classification/FeatureExtraction/NewCompositeExtractor.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2023 Richard Steinmetz <richard@steinmetz.cloud>
+ *
+ * @author Richard Steinmetz <richard@steinmetz.cloud>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\Mail\Service\Classification\FeatureExtraction;
+
+class NewCompositeExtractor extends CompositeExtractor {
+	public function __construct(VanillaCompositeExtractor      $ex1,
+								SubjectAndPreviewTextExtractor $ex2) {
+		parent::__construct([$ex1, $ex2]);
+	}
+}
