30fdfed7989fe13feaf8c1146cc2b17a928f9811
Christoph Wurst <christoph@winzerhof-wurst.at>
2023-01-27 10:16:25 +0100

    fixup! feat(importance-classifier): Reduce text feature vector
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Service/Classification/ImportanceClassifier.php
+++ lib/Service/Classification/ImportanceClassifier.php
@@ -41,12 +41,14 @@ use OCP\AppFramework\Db\DoesNotExistException;
 use Psr\Log\LoggerInterface;
 use Rubix\ML\Classifiers\ClassificationTree;
 use Rubix\ML\Classifiers\GaussianNB;
+use Rubix\ML\Classifiers\KNearestNeighbors;
 use Rubix\ML\Classifiers\MultilayerPerceptron;
 use Rubix\ML\Classifiers\RandomForest;
 use Rubix\ML\CrossValidation\Reports\MulticlassBreakdown;
 use Rubix\ML\Datasets\Labeled;
 use Rubix\ML\Datasets\Unlabeled;
 use Rubix\ML\Estimator;
+use Rubix\ML\Kernels\Distance\Manhattan;
 use Rubix\ML\NeuralNet\ActivationFunctions\Sigmoid;
 use Rubix\ML\NeuralNet\Layers\Activation;
 use Rubix\ML\NeuralNet\Layers\Dense;
@@ -105,7 +107,7 @@ class ImportanceClassifier {
 	/**
 	 * The maximum number of data sets to train the classifier with
 	 */
-	private const MAX_TRAINING_SET_SIZE = 1000;
+	private const MAX_TRAINING_SET_SIZE = 2000;
 
 	/** @var MailboxMapper */
 	private $mailboxMapper;
@@ -366,12 +368,13 @@ class ImportanceClassifier {
 
 	private function trainClassifier(array $trainingSet): Estimator {
 		//$classifier = new GaussianNB();
-		$classifier = new RandomForest(
+		/*$classifier = new RandomForest(
 			new ClassificationTree(10, 1),
 			10,
 			0.2,
 			true,
-		);
+		);*/
+		$classifier = new KNearestNeighbors(3, false, new Manhattan());
 		/*$classifier = new MultilayerPerceptron(
 			[
 				new Dense(1004),
