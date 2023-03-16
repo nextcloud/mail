1a9f59e89034a9c2cbbb7e0c8d1a63e9ee091b64
hamza221 <hamzamahjoubi221@gmail.com>
2023-03-16 14:28:06 +0100

    improve message preview
    Signed-off-by: hamza221 <hamzamahjoubi221@gmail.com>

--- lib/IMAP/MessageMapper.php
+++ lib/IMAP/MessageMapper.php
@@ -888,22 +888,6 @@ class MessageMapper {
 			if ($part === null) {
 				return new MessageStructureData($hasAttachments, $text, $isImipMessage, $isEncrypted);
 			}
-			$htmlBody = ($htmlBodyId !== null) ? $part->getBodyPart($htmlBodyId) : null;
-			if (!empty($htmlBody)) {
-				$mimeHeaders = $part->getMimeHeader($htmlBodyId, Horde_Imap_Client_Data_Fetch::HEADER_PARSE);
-				if ($enc = $mimeHeaders->getValue('content-transfer-encoding')) {
-					$structure->setTransferEncoding($enc);
-					$structure->setContents($htmlBody);
-					$htmlBody = $structure->getContents();
-				}
-				$html = new Html2Text($htmlBody);
-				return new MessageStructureData(
-					$hasAttachments,
-					trim($html->getText()),
-					$isImipMessage,
-					$isEncrypted,
-				);
-			}
 			$textBody = $part->getBodyPart($textBodyId);
 			if (!empty($textBody)) {
 				$mimeHeaders = $part->getMimeHeader($textBodyId, Horde_Imap_Client_Data_Fetch::HEADER_PARSE);
@@ -920,6 +904,23 @@ class MessageMapper {
 				);
 			}
 
+			$htmlBody = ($htmlBodyId !== null) ? $part->getBodyPart($htmlBodyId) : null;
+			if (!empty($htmlBody)) {
+				$mimeHeaders = $part->getMimeHeader($htmlBodyId, Horde_Imap_Client_Data_Fetch::HEADER_PARSE);
+				if ($enc = $mimeHeaders->getValue('content-transfer-encoding')) {
+					$structure->setTransferEncoding($enc);
+					$structure->setContents($htmlBody);
+					$htmlBody = $structure->getContents();
+				}
+				// TODO:  add 'alt_image' => 'hide' once it's added to the Html2Text package
+				$html = new Html2Text($htmlBody, array('do_links' => 'none',));
+				return new MessageStructureData(
+					$hasAttachments,
+					trim($html->getText()),
+					$isImipMessage,
+					$isEncrypted,
+				);
+			}
 			return new MessageStructureData($hasAttachments, $text, $isImipMessage, $isEncrypted);
 		}, iterator_to_array($structures->getIterator()));
 	}
