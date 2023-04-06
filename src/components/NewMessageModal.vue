8f6ca4f3ea2d24ec453f0464f86c9e8b034b8dd2
Richard Steinmetz <richard@steinmetz.cloud>
2023-04-06 17:22:14 +0200

    fix(drafts): delete old draft when saving new version
    The computed prop this.composerData?.draftId is not available in data()
    so it needs to be initialized in created().
    
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- src/components/NewMessageModal.vue
+++ src/components/NewMessageModal.vue
@@ -43,7 +43,6 @@
 			:in-reply-to-message-id="composerData.inReplyToMessageId"
 			:reply-to="composerData.replyTo"
 			:forward-from="composerData.forwardFrom"
-			:draft-id="composerData.draftId"
 			:send-at="composerData.sendAt * 1000"
 			:forwarded-messages="forwardedMessages"
 			:can-save-draft="canSaveDraft"
@@ -88,7 +87,7 @@ export default {
 	data() {
 		return {
 			original: undefined,
-			draftsPromise: Promise.resolve(this.composerData?.draftId),
+			draftsPromise: Promise.resolve(),
 			attachmentsPromise: Promise.resolve(),
 			canSaveDraft: true,
 			savingDraft: false,
@@ -97,6 +96,12 @@ export default {
 			sending: false,
 			error: undefined,
 			warning: undefined,
+		}
+	},
+	created() {
+		const draftId = this.composerData?.draftId
+		if (draftId) {
+			this.draftsPromise = Promise.resolve(draftId)
 		}
 	},
 	computed: {
