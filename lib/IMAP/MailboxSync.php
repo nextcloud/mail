f4b01d9fa8adee1825abcc1c3a32268dc98c4856
Christoph Wurst <christoph@winzerhof-wurst.at>
2023-05-24 10:16:48 +0200

    perf(imap): Reduce number of STATUS commands for background mailbox sync
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/IMAP/MailboxSync.php
+++ lib/IMAP/MailboxSync.php
@@ -43,8 +43,11 @@ use OCP\IDBConnection;
 use Psr\Log\LoggerInterface;
 use function array_combine;
 use function array_map;
+use function array_reduce;
+use function array_slice;
 use function in_array;
 use function json_encode;
+use function shuffle;
 use function sprintf;
 use function str_starts_with;
 
@@ -103,14 +106,16 @@ class MailboxSync {
 				$namespaces = $client->getNamespaces([], [
 					'ob_return' => true,
 				]);
+				$personalNamespace = $this->getPersonalNamespace($namespaces);
 				$account->getMailAccount()->setPersonalNamespace(
-					$this->getPersonalNamespace($namespaces)
+					$personalNamespace
 				);
 			} catch (Horde_Imap_Client_Exception $e) {
 				$logger->debug('Getting namespaces for account ' . $account->getId() . ' failed: ' . $e->getMessage(), [
 					'exception' => $e,
 				]);
 				$namespaces = null;
+				$personalNamespace = null;
 			}
 
 			try {
@@ -125,7 +130,7 @@ class MailboxSync {
 			}
 			$this->folderMapper->detectFolderSpecialUse($folders);
 
-			$this->atomic(function () use ($account, $folders, $namespaces) {
+			$mailboxes = $this->atomic(function () use ($account, $folders, $namespaces) {
 				$old = $this->mailboxMapper->findAll($account);
 				$indexedOld = array_combine(
 					array_map(static function (Mailbox $mb) {
@@ -134,8 +139,10 @@ class MailboxSync {
 					$old
 				);
 
-				$this->persist($account, $folders, $indexedOld, $namespaces);
+				return $this->persist($account, $folders, $indexedOld, $namespaces);
 			}, $this->dbConnection);
+
+			$this->syncMailboxStatus($mailboxes, $personalNamespace, $client);
 
 			$this->dispatcher->dispatchTyped(
 				new MailboxesSynchronizedEvent($account)
@@ -156,7 +163,7 @@ class MailboxSync {
 	public function syncStats(Account $account, Mailbox $mailbox): void {
 		$client = $this->imapClientFactory->getClient($account);
 		try {
-			$stats = $this->folderMapper->getFoldersStatusAsObject($client, $mailbox->getName());
+			$stats = $this->folderMapper->getFoldersStatusAsObject($client, [$mailbox->getName()])[$mailbox->getName()];
 		} catch (Horde_Imap_Client_Exception $e) {
 			$id = $mailbox->getId();
 			throw new ServiceException(
@@ -173,19 +180,23 @@ class MailboxSync {
 		$this->mailboxMapper->update($mailbox);
 	}
 
+	/**
+	 * @return Mailbox[]
+	 */
 	private function persist(Account $account,
 		array $folders,
 		array $existing,
-		?Horde_Imap_Client_Namespace_List $namespaces): void {
+		?Horde_Imap_Client_Namespace_List $namespaces): array {
+		$mailboxes = [];
 		foreach ($folders as $folder) {
 			if (isset($existing[$folder->getMailbox()])) {
-				$this->updateMailboxFromFolder(
+				$mailboxes[] = $this->updateMailboxFromFolder(
 					$folder,
 					$existing[$folder->getMailbox()],
 					$namespaces,
 				);
 			} else {
-				$this->createMailboxFromFolder(
+				$mailboxes[] = $this->createMailboxFromFolder(
 					$account,
 					$folder,
 					$namespaces,
@@ -201,44 +212,49 @@ class MailboxSync {
 
 		$account->getMailAccount()->setLastMailboxSync($this->timeFactory->getTime());
 		$this->mailAccountMapper->update($account->getMailAccount());
+
+		return $mailboxes;
 	}
 
 	private function getPersonalNamespace(Horde_Imap_Client_Namespace_List $namespaces): ?string {
 		foreach ($namespaces as $namespace) {
 			/** @var Horde_Imap_Client_Data_Namespace $namespace */
 			if ($namespace->type === Horde_Imap_Client::NS_PERSONAL) {
-				return $namespace->name;
+				return $namespace->name !== "" ? $namespace->name : null;
 			}
-			$namespace->name;
 		}
 		return null;
 	}
 
-	private function updateMailboxFromFolder(Folder $folder, Mailbox $mailbox, ?Horde_Imap_Client_Namespace_List $namespaces): void {
+	private function updateMailboxFromFolder(Folder $folder, Mailbox $mailbox, ?Horde_Imap_Client_Namespace_List $namespaces): Mailbox {
 		$mailbox->setAttributes(json_encode($folder->getAttributes()));
 		$mailbox->setDelimiter($folder->getDelimiter());
-		$mailbox->setMessages($folder->getStatus()['messages'] ?? 0);
-		$mailbox->setUnseen($folder->getStatus()['unseen'] ?? 0);
+		$status = $folder->getStatus();
+		if ($status !== null) {
+			$mailbox->setMessages($status['messages'] ?? 0);
+			$mailbox->setUnseen($status['unseen'] ?? 0);
+		}
 		$mailbox->setSelectable(!in_array('\noselect', $folder->getAttributes()));
 		$mailbox->setSpecialUse(json_encode($folder->getSpecialUse()));
 		$mailbox->setMyAcls($folder->getMyAcls());
 		$mailbox->setShared($this->isMailboxShared($namespaces, $mailbox));
-		$this->mailboxMapper->update($mailbox);
+		return $this->mailboxMapper->update($mailbox);
 	}
 
-	private function createMailboxFromFolder(Account $account, Folder $folder, ?Horde_Imap_Client_Namespace_List $namespaces): void {
+	private function createMailboxFromFolder(Account $account, Folder $folder, ?Horde_Imap_Client_Namespace_List $namespaces): Mailbox {
 		$mailbox = new Mailbox();
 		$mailbox->setName($folder->getMailbox());
 		$mailbox->setAccountId($account->getId());
 		$mailbox->setAttributes(json_encode($folder->getAttributes()));
 		$mailbox->setDelimiter($folder->getDelimiter());
-		$mailbox->setMessages($folder->getStatus()['messages'] ?? 0);
-		$mailbox->setUnseen($folder->getStatus()['unseen'] ?? 0);
+		$status = $folder->getStatus();
+		$mailbox->setMessages($status['messages'] ?? 0);
+		$mailbox->setUnseen($status['unseen'] ?? 0);
 		$mailbox->setSelectable(!in_array('\noselect', $folder->getAttributes()));
 		$mailbox->setSpecialUse(json_encode($folder->getSpecialUse()));
 		$mailbox->setMyAcls($folder->getMyAcls());
 		$mailbox->setShared($this->isMailboxShared($namespaces, $mailbox));
-		$this->mailboxMapper->insert($mailbox);
+		return $this->mailboxMapper->insert($mailbox);
 	}
 
 	private function isMailboxShared(?Horde_Imap_Client_Namespace_List $namespaces, Mailbox $mailbox): bool {
@@ -249,5 +265,42 @@ class MailboxSync {
 			}
 		}
 		return false;
+	}
+
+	private function syncMailboxStatus(mixed $mailboxes, ?string $personalNamespace, \Horde_Imap_Client_Socket $client): void {
+		/** @var array{0: Mailbox[], 1: Mailbox[]} */
+		[$sync, $doNotSync] = array_reduce($mailboxes, function (array $carry, Mailbox $mailbox) use ($personalNamespace): array {
+			/** @var array{0: Mailbox[], 1: Mailbox[]} $carry */
+			[$sync, $doNotSync] = $carry;
+			$inboxName = $personalNamespace === null ? "INBOX" : ($personalNamespace . $mailbox->getDelimiter() . "INBOX");
+			if ($inboxName === $mailbox->getName() || $mailbox->getSyncInBackground()) {
+				return [
+					array_merge($sync, [$mailbox]),
+					$doNotSync,
+				];
+			}
+			return [
+				$sync,
+				array_merge($doNotSync, [$mailbox]),
+			];
+		}, [[], []]);
+
+		// Synchronize the mailboxes selected for sync and keep the rest updated occasionally
+		shuffle($doNotSync);
+		/** @var Mailbox[] $syncStatus */
+		$syncStatus = [...$sync, ...array_slice($doNotSync, 0, 5)];
+		$statuses = $this->folderMapper->getFoldersStatusAsObject($client, array_map(function (Mailbox $mailbox) {
+			return $mailbox->getName();
+		}, $syncStatus));
+		foreach ($syncStatus as $mailbox) {
+			$status = $statuses[$mailbox->getName()];
+			$mailbox->setMessages($status->getTotal());
+			$mailbox->setUnseen($status->getUnread());
+		}
+		$this->atomic(function () use ($syncStatus) {
+			foreach ($syncStatus as $mailbox) {
+				$this->mailboxMapper->update($mailbox);
+			}
+		}, $this->dbConnection);
 	}
 }
