30285fb250cedd72951581963694f6f3067b6f3c
hamza221 <hamzamahjoubi221@gmail.com>
2023-07-03 19:08:46 +0200

    Add slash command to mail composer
    Signed-off-by: hamza221 <hamzamahjoubi221@gmail.com>

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -53,7 +53,9 @@ import ImagePlugin from '@ckeditor/ckeditor5-image/src/image'
 import ImageResizePlugin from '@ckeditor/ckeditor5-image/src/imageresize'
 import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload'
 import MailPlugin from '../ckeditor/mail/MailPlugin'
-
+import Tribute from 'tributejs/dist/tribute.esm.js'
+import { searchProvider, getLinkWithPicker } from '@nextcloud/vue/dist/Components/NcRichText'
+import { emojiSearch, emojiAddRecent } from '@nextcloud/vue/dist/Functions/emoji'
 import { getLanguage } from '@nextcloud/l10n'
 
 import logger from '../logger'
@@ -131,6 +133,9 @@ export default {
 		}
 
 		return {
+			linkTribute: null,
+			emojiTribute: null,
+			textSmiles: [],
 			ready: false,
 			editor: Editor,
 			config: {
@@ -140,6 +145,20 @@ export default {
 					items: toolbar,
 				},
 				language: 'en',
+				mention: {
+					feeds: [
+						{
+							marker: ':',
+							feed: this.getEmoji,
+							itemRenderer: this.customEmojiRenderer,
+						},
+						{
+							marker: '/',
+							feed: this.getLink,
+							itemRenderer: this.customLinkRenderer,
+						},
+					],
+				},
 			},
 		}
 	},
@@ -147,6 +166,65 @@ export default {
 		this.loadEditorTranslations(getLanguage())
 	},
 	methods: {
+		getLink(text) {
+			const results = searchProvider(text)
+			if (results.length === 1 && !results[0].title.toLowerCase().includes(text.toLowerCase())) {
+				return []
+			}
+			return results
+		},
+		getEmoji(text) {
+			const emojiResults = emojiSearch(text)
+			if (this.textSmiles.includes(':' + text)) {
+
+				emojiResults.unshift(':' + text)
+			}
+			return emojiResults
+		},
+		 customEmojiRenderer(item) {
+			const itemElement = document.createElement('span')
+
+			itemElement.classList.add('custom-item')
+			itemElement.id = `mention-list-item-id-${item.colons}`
+			itemElement.textContent = `${item.native} `
+			itemElement.style.width = '100%'
+			itemElement.style.borderRadius = '8px'
+			itemElement.style.padding = '4px 8px'
+			itemElement.style.display = 'block'
+
+			const usernameElement = document.createElement('span')
+
+			usernameElement.classList.add('custom-item-username')
+			usernameElement.textContent = item.colons
+
+			itemElement.appendChild(usernameElement)
+
+			return itemElement
+		},
+		customLinkRenderer(item) {
+			const itemElement = document.createElement('span')
+			itemElement.classList.add('link-container')
+			itemElement.style.width = '100%'
+			itemElement.style.borderRadius = '8px'
+			itemElement.style.padding = '4px 8px'
+			itemElement.style.display = 'block'
+
+			const icon = document.createElement('img')
+			icon.style.width = '20px'
+			icon.style.marginRight = '1em'
+			icon.style.filter = 'var(--background-invert-if-dark)'
+			icon.classList.add('link-icon')
+			icon.src = `${item.icon_url} `
+
+			const usernameElement = document.createElement('span')
+
+			usernameElement.classList.add('link-title')
+			usernameElement.textContent = `${item.title} `
+			itemElement.appendChild(icon)
+			itemElement.appendChild(usernameElement)
+
+			return itemElement
+		},
 		async loadEditorTranslations(language) {
 			if (language === 'en') {
 				// The default, nothing to fetch
@@ -178,7 +256,106 @@ export default {
 		 */
 		onEditorReady(editor) {
 			logger.debug('TextEditor is ready', { editor })
+
+			editor.commands.get('mention')?.on('execute', (event, data) => {
+				event.stop()
+				const eventData = data[0]
+				const item = eventData.mention
+				if (eventData.marker === ':') {
+					emojiAddRecent(item)
+					this.editorInstance.execute('insertItem', item.native, ':')
+				}
+				if (eventData.marker === '/') {
+					getLinkWithPicker(item.id)
+						.then((link) => {
+							this.editorInstance.execute('insertItem', link, '/')
+							this.editorInstance.editing.view.focus()
+						})
+						.catch((error) => {
+							console.debug('Smart picker promise rejected:', error)
+						})
+				}
+			}, { priority: 'high' })
 			this.editorInstance = editor
+			const linkOptions = {
+				trigger: '/',
+				// Don't use the tribute search function at all
+				// We pass search results as values (see below)
+				lookup: (result, query) => query,
+				// Where to inject the menu popup
+				menuContainer: document.querySelector('.modal-mask'),
+				// Popup mention autocompletion templates
+				menuItemTemplate: item => `<img class="tribute-container-link__item__icon" src="${item.original.icon_url}"> <span class="tribute-container-link__item__label">${item.original.title}</span>`,
+				// Hide if no results
+				noMatchTemplate: () => t('No link provider found'),
+				selectTemplate: this.getLink,
+				// Pass the search results as values
+				values: (text, cb) => cb(searchProvider(text)),
+				// Class added to the menu container
+				containerClass: 'tribute-container-link',
+				// Class added to each list item
+				itemClass: 'tribute-container-link__item',
+			}
+			const emojiOptions = {
+				trigger: ':',
+				// Don't use the tribute search function at all
+				// We pass search results as values (see below)
+				lookup: (result, query) => query,
+				// Where to inject the menu popup
+				menuContainer: document.querySelector('.modal-mask'),
+				// Popup mention autocompletion templates
+				menuItemTemplate: item => {
+					if (this.textSmiles.includes(item.original)) {
+						// Display the raw text string for :), :-D, … for non emoji results,
+						// instead of trying to show an image and their name.
+						return item.original
+					}
+
+					return `<span class="tribute-container-emoji__item__emoji">${item.original.native}</span> :${item.original.short_name}`
+				},
+				// Hide if no results
+				noMatchTemplate: () => t('No emoji found'),
+				// Display raw emoji along with its name
+				selectTemplate: (item) => {
+					if (this.textSmiles.includes(item.original)) {
+						// Replace the selection with the raw text string for :), :-D, … for non emoji results
+						this.editorInstance.execute('delete')
+						this.appendToBodyAtCursor(item.original)
+					}
+
+					emojiAddRecent(item.original)
+					this.editorInstance.execute('delete')
+					this.appendToBodyAtCursor(item.original.native)
+				},
+				// Pass the search results as values
+				values: (text, cb) => {
+					const emojiResults = emojiSearch(text)
+					if (this.textSmiles.includes(':' + text)) {
+						/**
+						 * Prepend text smiles to the search results so that Tribute
+						 * is not interfering with normal writing, aka. "Cocos Island Meme".
+						 * E.g. `:)` and `:-)` got replaced by the flag of Cocos Island,
+						 * when submitting the input with Enter after writing them
+						 */
+						emojiResults.unshift(':' + text)
+					}
+					cb(emojiResults)
+				},
+				// Class added to the menu container
+				containerClass: 'tribute-container-emoji',
+				// Class added to each list item
+				itemClass: 'tribute-container-emoji__item',
+			}
+
+			this.linkTribute = new Tribute(linkOptions)
+			this.emojiTribute = new Tribute(emojiOptions)
+
+			// To solve failing unit test
+			// [Tribute] Must pass in a DOM node or NodeList.
+			if (editor.sourceElement) {
+				this.linkTribute.attach(editor.sourceElement)
+				this.emojiTribute.attach(editor.sourceElement)
+			}
 
 			if (this.focus) {
 				logger.debug('focusing TextEditor')
@@ -215,7 +392,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 :deep(a) {
 	color: #07d;
 }
