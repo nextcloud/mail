ad6b723e18f16c879b47d5dd051d0fe67718bb98
Johannes Merkel <mail@johannesgge.de>
2023-08-18 11:42:23 +0200

    fix(Snooze): Sort snooze mailbox as specialUse
    Signed-off-by: Johannes Merkel <mail@johannesgge.de>

--- src/tests/unit/imap/MailboxSorter.spec.js
+++ src/tests/unit/imap/MailboxSorter.spec.js
@@ -26,14 +26,20 @@ describe('mailboxSorter', () => {
 		const mb1 = {
 			name: 'Inbox 1',
 			specialUse: [],
+			databaseId: 1,
 		}
 		const mb2 = {
 			name: 'Inbox 2',
 			specialUse: [],
+			databaseId: 2,
 		}
 		const mailboxes = [mb2, mb1]
 
-		const sorted = sortMailboxes(mailboxes)
+		const account = {
+			snoozeMailboxId: 0,
+		}
+
+		const sorted = sortMailboxes(mailboxes, account)
 
 		expect(sorted).toEqual([mb1, mb2])
 	})
@@ -42,14 +48,20 @@ describe('mailboxSorter', () => {
 		const mb1 = {
 			name: 'Inbox 1',
 			specialUse: [],
+			databaseId: 1,
 		}
 		const mb2 = {
 			name: 'Inbox 2',
 			specialUse: ['inbox'],
+			databaseId: 2,
 		}
 		const mailboxes = [mb1, mb2]
 
-		const sorted = sortMailboxes(mailboxes)
+		const account = {
+			snoozeMailboxId: 0,
+		}
+
+		const sorted = sortMailboxes(mailboxes, account)
 
 		expect(sorted).toEqual([mb2, mb1])
 	})
@@ -58,14 +70,20 @@ describe('mailboxSorter', () => {
 		const mb1 = {
 			name: 'Inbox 1',
 			specialUse: ['inbox'],
+			databaseId: 1,
 		}
 		const mb2 = {
 			name: 'Inbox 2',
 			specialUse: ['inbox'],
+			databaseId: 2,
 		}
 		const mailboxes = [mb1, mb2]
 
-		const sorted = sortMailboxes(mailboxes)
+		const account = {
+			snoozeMailboxId: 0,
+		}
+
+		const sorted = sortMailboxes(mailboxes, account)
 
 		expect(sorted).toEqual([mb1, mb2])
 	})
@@ -74,31 +92,46 @@ describe('mailboxSorter', () => {
 		const mb1 = {
 			name: 'Drafts',
 			specialUse: ['drafts'],
+			databaseId: 2,
 		}
 		const mb2 = {
 			name: 'Inbox',
 			specialUse: ['inbox'],
+			databaseId: 1,
 		}
 		const mb3 = {
 			name: 'Other 2',
 			specialUse: [],
+			databaseId: 3,
 		}
 		const mb4 = {
 			name: 'Other 1',
 			specialUse: [],
+			databaseId: 4,
 		}
 		const mb5 = {
 			name: 'Sent',
 			specialUse: ['sent'],
+			databaseId: 5,
 		}
 		const mb6 = {
 			name: 'Sent2',
 			specialUse: ['sent'],
+			databaseId: 6,
 		}
-		const mailboxes = [mb1, mb2, mb3, mb4, mb5, mb6]
+		const mb7 = {
+			name: 'Snoozed',
+			specialUse: [],
+			databaseId: 7,
+		}
+		const mailboxes = [mb1, mb2, mb3, mb4, mb5, mb6, mb7]
 
-		const sorted = sortMailboxes(mailboxes)
+		const account = {
+			snoozeMailboxId: 7,
+		}
 
-		expect(sorted).toEqual([mb2, mb1, mb5, mb6, mb4, mb3])
+		const sorted = sortMailboxes(mailboxes, account)
+
+		expect(sorted).toEqual([mb2, mb1, mb5, mb6, mb7, mb4, mb3])
 	})
 })
