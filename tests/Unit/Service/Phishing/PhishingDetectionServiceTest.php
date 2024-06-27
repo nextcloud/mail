d7c6108adc3498b6c7c6ace7f99188f6866eeec9
Hamza Mahjoubi <hamzamahjoubi221@gmail.com>
2024-06-27 03:15:50 +0200

    fixup! Feat: phishing detection
    Signed-off-by: Hamza Mahjoubi <hamzamahjoubi221@gmail.com>

--- tests/Unit/Service/Phishing/PhishingDetectionServiceTest.php
+++ tests/Unit/Service/Phishing/PhishingDetectionServiceTest.php
@@ -1,45 +1,25 @@
 <?php
 /**
- * @copyright 2024 Hamza Mahjoubi <hamza.mahjoubi221@proton.me>
- *
- * @author  2024 Hamza Mahjoubi <hamza.mahjoubi221@proton.me>
- *
- * @license GNU AGPL version 3 or any later version
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\Mail\Tests\Integration\Service\Phishing;
+namespace OCA\Mail\Tests\Unit\Service\Phishing;
 
 use ChristophWurst\Nextcloud\Testing\TestCase;
 use Horde_Mime_Headers;
 use OCA\Mail\PhishingDetectionResult;
 
-use OCA\Mail\Service\ContactsIntegration;
 use OCA\Mail\Service\PhishingDetection\ContactCheck;
 use OCA\Mail\Service\PhishingDetection\CustomEmailCheck;
 use OCA\Mail\Service\PhishingDetection\DateCheck;
 use OCA\Mail\Service\PhishingDetection\PhishingDetectionService;
 use OCA\Mail\Service\PhishingDetection\ReplyToCheck;
-use OCP\IL10N;
 
 use PHPUnit\Framework\MockObject\MockObject;
 
 class PhishingDetectionServiceTest extends TestCase {
 
-	private ContactsIntegration|MockObject $contactsIntegration;
-	private IL10N|MockObject $l10n;
 	private ContactCheck|MockObject $contactCheck;
 	private CustomEmailCheck|MockObject $customEmailCheck;
 	private DateCheck|MockObject $dateCheck;
@@ -48,8 +28,6 @@ class PhishingDetectionServiceTest extends TestCase {
 
 	protected function setUp(): void {
 		parent::setUp();
-		$this->contactsIntegration = $this->createMock(ContactsIntegration::class);
-		$this->l10n = $this->createMock(IL10N::class);
 		$this->contactCheck = $this->createMock(ContactCheck::class);
 		$this->customEmailCheck = $this->createMock(customEmailCheck::class);
 		$this->dateCheck = $this->createMock(DateCheck::class);
@@ -60,7 +38,7 @@ class PhishingDetectionServiceTest extends TestCase {
 	
 
 	public function testCheckHeadersForPhishing(): void {
-		$headerStream = fopen(__DIR__ . '/../../data/phishing-mail-headers.txt', 'r');
+		$headerStream = fopen(__DIR__ . '/../../../data/phishing-mail-headers.txt', 'r');
 		$parsedHeaders = Horde_Mime_Headers::parseHeaders($headerStream);
 		fclose($headerStream);
 		$this->replyToCheck->expects($this->once())
