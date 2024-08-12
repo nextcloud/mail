6d06761d58fd1359d6659ea2ce18d9b4b4134a0d
Richard Steinmetz <richard@steinmetz.cloud>
2024-08-12 09:09:19 +0200

    chore: bump psr/log to ^2
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- lib/Support/ConsoleLoggerDecorator.php
+++ lib/Support/ConsoleLoggerDecorator.php
@@ -25,49 +25,49 @@ class ConsoleLoggerDecorator implements LoggerInterface {
 		$this->consoleOutput = $consoleOutput;
 	}
 
-	public function emergency($message, array $context = []) {
+	public function emergency(string|\Stringable $message, array $context = []) {
 		$this->consoleOutput->writeln("<error>[emergency] $message</error>");
 
 		$this->inner->emergency($message, $context);
 	}
 
-	public function alert($message, array $context = []) {
+	public function alert(string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("<error>[alert] $message</error>");
 
 		$this->inner->alert($message, $context);
 	}
 
-	public function critical($message, array $context = []) {
+	public function critical(string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("<error>[critical] $message</error>");
 
 		$this->inner->critical($message, $context);
 	}
 
-	public function error($message, array $context = []) {
+	public function error(string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("<error>[error] $message</error>");
 
 		$this->inner->error($message, $context);
 	}
 
-	public function warning($message, array $context = []) {
+	public function warning(string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("[warning] $message");
 
 		$this->inner->warning($message, $context);
 	}
 
-	public function notice($message, array $context = []) {
+	public function notice(string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("<info>[notice] $message</info>");
 
 		$this->inner->notice($message, $context);
 	}
 
-	public function info($message, array $context = []) {
+	public function info(string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("<info>[info] $message</info>");
 
 		$this->inner->info($message, $context);
 	}
 
-	public function debug($message, array $context = []) {
+	public function debug(string|\Stringable $message, array $context = []): void {
 		if ($this->consoleOutput->getVerbosity() < OutputInterface::VERBOSITY_DEBUG) {
 			return;
 		}
@@ -77,7 +77,7 @@ class ConsoleLoggerDecorator implements LoggerInterface {
 		$this->inner->debug($message, $context);
 	}
 
-	public function log($level, $message, array $context = []) {
+	public function log($level, string|\Stringable $message, array $context = []): void {
 		$this->consoleOutput->writeln("<info>[log] $message</info>");
 
 		$this->inner->log($level, $message, $context);
