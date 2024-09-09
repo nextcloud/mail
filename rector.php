263b09c2f79dc2d4e2b325d4c3c0fbfcbbbabe67
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-09-09 11:03:23 +0200

    chore(rector): Upgrade config file
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- rector.php
+++ rector.php
@@ -1,18 +1,19 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 use Rector\Config\RectorConfig;
 
-return static function (RectorConfig $rectorConfig): void {
-	$rectorConfig->paths([
+return RectorConfig::configure()
+	->withPaths([
 		__DIR__ . '/lib',
 		__DIR__ . '/tests',
+	])
+	->withSkip([
+		__DIR__ . '/lib/Vendor'
 	]);
-	$rectorConfig->skip([
-		__DIR__ . '/lib/Vendor',
-	]);
-};
