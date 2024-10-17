1ed5c2a12f97986e50e1643447974a2ee51736c3
Hamza Mahjoubi <hamzamahjoubi221@gmail.com>
2024-10-17 19:08:20 +0200

    feat: mail snippets
    Signed-off-by: Hamza Mahjoubi <hamzamahjoubi221@gmail.com>

---
+++ src/components/snippets/List.vue
@@ -0,0 +1,29 @@
+<!--
+  - SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<template>
+</template>
+
+<script>
+
+export default {
+	name: 'List',
+	components: {
+
+	},
+	props: {
+	},
+	data() {
+	},
+	mounted() {
+	},
+	updated() {
+	},
+	methods: {
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
