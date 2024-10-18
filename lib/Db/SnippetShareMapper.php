722b0b1c5dbb8d711d8f5b6ab1aaf30d6f07b73c
Hamza Mahjoubi <hamzamahjoubi221@gmail.com>
2024-10-18 18:52:55 +0200

    fixup! feat: mail snippets
    Signed-off-by: Hamza Mahjoubi <hamzamahjoubi221@gmail.com>

--- lib/Db/SnippetShareMapper.php
+++ lib/Db/SnippetShareMapper.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
@@ -42,7 +42,7 @@ class SnippetShareMapper extends QBMapper {
 		return $this->findEntity($qb);
 	}
 
-	public function shareExists(string $snippetId, string $shareWith): bool {
+	public function shareExists(int $snippetId, string $shareWith): bool {
 		$qb = $this->db->getQueryBuilder();
 		$qb->select('*')
 			->from($this->getTableName())
