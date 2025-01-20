d856a6005c453e2cd91666941897aed3ca9cbf33
Richard Steinmetz <richard@steinmetz.cloud>
2025-01-20 11:07:47 +0100

    fix(migration): make misc migrations idempotent
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- lib/Migration/Version4000Date20240716172702.php
+++ lib/Migration/Version4000Date20240716172702.php
@@ -25,28 +25,30 @@ class Version4000Date20240716172702 extends SimpleMigrationStep {
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
-		$table = $schema->createTable('mail_internal_address');
 
-		$table->addColumn('id', Types::INTEGER, [
-			'autoincrement' => true,
-			'notnull' => true,
-			'length' => 4,
-		]);
-		$table->addColumn('address', Types::STRING, [
-			'notnull' => true,
-			'length' => 255,
-		]);
-		$table->addColumn('type', Types::STRING, [
-			'notnull' => true,
-			'length' => 64,
-		]);
-		$table->addColumn('user_id', Types::STRING, [
-			'notnull' => true,
-			'length' => 64,
-		]);
-		$table->setPrimaryKey(['id']);
-		$table->addUniqueIndex(['address', 'user_id'], 'mail_internal_address_uniq');
-		
+		if (!$schema->hasTable('mail_internal_address')) {
+			$table = $schema->createTable('mail_internal_address');
+			$table->addColumn('id', Types::INTEGER, [
+				'autoincrement' => true,
+				'notnull' => true,
+				'length' => 4,
+			]);
+			$table->addColumn('address', Types::STRING, [
+				'notnull' => true,
+				'length' => 255,
+			]);
+			$table->addColumn('type', Types::STRING, [
+				'notnull' => true,
+				'length' => 64,
+			]);
+			$table->addColumn('user_id', Types::STRING, [
+				'notnull' => true,
+				'length' => 64,
+			]);
+			$table->setPrimaryKey(['id']);
+			$table->addUniqueIndex(['address', 'user_id'], 'mail_internal_address_uniq');
+		}
+
 		return $schema;
 	}
 
