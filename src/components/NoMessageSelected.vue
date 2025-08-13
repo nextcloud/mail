85f06283836e7243496c20d37d2565aafcb0f6ea
Christoph Wurst <christoph@winzerhof-wurst.at>
2025-08-13 16:13:03 +0200

    feat(ui): add fluid design
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/components/NoMessageSelected.vue
+++ src/components/NoMessageSelected.vue
@@ -4,23 +4,25 @@
 -->
 
 <template>
-	<AppContentDetails class="app-content">
-		<NcEmptyContent class="app-content__empty"
-			:name="t('mail', 'Welcome to {productName} Mail', { productName }, null, {escape: false})"
-			:description="t('mail', 'Start writing a message by clicking below or select an existing message to display its contents')">
-			<template #icon>
-				<IconMail />
-			</template>
-			<template #action>
-				<NewMessageButtonHeader />
-			</template>
-		</NcEmptyContent>
+	<AppContentDetails class="app-content no-message-selected"
+		:style="{ 'backgroundImage': backgroundImgSrc }">
+		<div class="no-message-selected__heading">
+			{{ t('mail', 'Welcome to {productName} Mail', { productName }, null, {escape: false}) }}
+		</div>
+		<div class="no-message-selected__text">
+			{{ t('mail', 'Start writing a message by clicking below or select an existing message to display its contents') }}
+		</div>
+		<div class="no-message-selected__action">
+			<NewMessageButtonHeader :show-refresh="false" />
+		</div>
 	</AppContentDetails>
 </template>
 
 <script>
-import { NcAppContentDetails as AppContentDetails, NcEmptyContent } from '@nextcloud/vue'
-import IconMail from 'vue-material-design-icons/EmailOutline.vue'
+import { generateFilePath } from '@nextcloud/router'
+import { isDarkTheme } from '@nextcloud/vue/functions/isDarkTheme'
+import { NcAppContentDetails as AppContentDetails } from '@nextcloud/vue'
+
 import NewMessageButtonHeader from './NewMessageButtonHeader.vue'
 
 export default {
@@ -28,8 +30,14 @@ export default {
 	components: {
 		NewMessageButtonHeader,
 		AppContentDetails,
-		NcEmptyContent,
-		IconMail,
+	},
+
+	data() {
+		return {
+			backgroundImgSrc: isDarkTheme
+				? 'url("' + generateFilePath('mail', 'img', 'welcome-connection-dark.png') + '")'
+				: 'url("' + generateFilePath('mail', 'img', 'welcome-connection-light.png') + '")',
+		}
 	},
 
 	computed: {
@@ -40,21 +48,29 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-:deep(.refresh__button) {
-	display: none !important;
-}
-
-.header {
-	display: grid !important;
-	justify-content: center !important;
-}
-
-.app-content {
+.no-message-selected {
 	display: flex;
-	height: 100%;
+	flex-direction: column;
+	align-items: flex-start;
 	justify-content: center;
-	&__empty {
-		flex-grow: unset;
+	gap: calc(var(--default-grid-baseline, 4px) * 2);
+	padding-inline-start: 50px;
+	height: 100%;
+	max-width: 100% !important; /* restricted otherwise by stronger selector */
+
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: right 100% bottom 40%;
+
+	&__heading {
+		font-weight: bold;
+		font-size: 20px;
+		line-height: 30px;
+	}
+
+	&__text {
+		text-wrap-style: balance;
+		max-width: 50%;
 	}
 }
 </style>
