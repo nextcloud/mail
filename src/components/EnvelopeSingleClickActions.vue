69ed43eb448ff67e5950b9dc5d48c29a5a4bf34b
Andy Scherzinger <info@andy-scherzinger.de>
2025-08-27 10:29:40 +0200

    style(icon): Use material symbol variant of delete icon
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/components/EnvelopeSingleClickActions.vue
+++ src/components/EnvelopeSingleClickActions.vue
@@ -32,7 +32,7 @@
 import ImportantOutlineIcon from 'vue-material-design-icons/LabelVariantOutline.vue'
 import EmailUnread from 'vue-material-design-icons/EmailOutline.vue'
 import EmailRead from 'vue-material-design-icons/EmailOpenOutline.vue'
-import IconDelete from 'vue-material-design-icons/DeleteOutline.vue'
+import IconDelete from 'vue-material-design-icons/TrashCanOutline.vue'
 import ImportantIcon from 'vue-material-design-icons/LabelVariant.vue'
 
 export default {
