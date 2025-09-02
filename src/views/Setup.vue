4dc64595d8ba9717937c76366c3ad71cb766b686
Christoph Wurst <christoph@winzerhof-wurst.at>
2025-09-02 15:50:30 +0200

    fix(ui): use fluid background image for the setup view
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>
    
    [skip ci]

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -6,29 +6,35 @@
 	<NcContent app-name="mail">
 		<Navigation v-if="hasAccounts" />
 		<AppContent>
-			<EmptyContent v-if="allowNewMailAccounts"
-				class="setup__form-content"
-				:name="t('mail', 'Connect your mail account')">
-				<template #icon>
-					<div class="setup__form-content__svg-wrapper" v-html="FluidMail" />
-				</template>
-				<template #action>
-					<AccountForm :display-name="displayName"
-						:email="email"
-						:error.sync="error"
-						@account-created="onAccountCreated" />
-				</template>
-			</EmptyContent>
-			<EmptyContent v-else :name="t('mail', 'To add a mail account, please contact your administrator.')">
-				<template #icon>
-					<IconMail />
-				</template>
-			</EmptyContent>
+			<div class="setup"
+				:class="{ 'setup--themed': isThemed, }"
+				:style="{ 'backgroundImage': isThemed ? undefined: backgroundImgSrc, }">
+				<EmptyContent v-if="allowNewMailAccounts"
+					class="setup__form-content"
+					:name="t('mail', 'Connect your mail account')">
+					<template #icon>
+						<div class="setup__form-content__svg-wrapper" v-html="FluidMail" />
+					</template>
+					<template #action>
+						<AccountForm :display-name="displayName"
+							:email="email"
+							:error.sync="error"
+							class="setup__form-content__form"
+							@account-created="onAccountCreated" />
+					</template>
+				</EmptyContent>
+				<EmptyContent v-else :name="t('mail', 'To add a mail account, please contact your administrator.')">
+					<template #icon>
+						<div class="setup__form-content__svg-wrapper" v-html="FluidMail" />
+					</template>
+				</EmptyContent>
+			</div>
 		</AppContent>
 	</NcContent>
 </template>
 
 <script>
+import { generateFilePath } from '@nextcloud/router'
 import { NcContent, NcAppContent as AppContent, NcEmptyContent as EmptyContent } from '@nextcloud/vue'
 import { loadState } from '@nextcloud/initial-state'
 
@@ -55,6 +61,12 @@ export default {
 			FluidMail,
 			allowNewMailAccounts: loadState('mail', 'allow-new-accounts', true),
 			error: null,
+			backgroundImgSrc: this.isDarkTheme
+				? 'url("' + generateFilePath('mail', 'img', 'welcome-connection-dark.png') + '")'
+				: 'url("' + generateFilePath('mail', 'img', 'welcome-connection-light.png') + '")',
+			isThemed: this.isDarkTheme
+				? window.getComputedStyle(document.body).getPropertyValue('--color-primary-element') !== '#0091f2'
+				: window.getComputedStyle(document.body).getPropertyValue('--color-primary-element') !== '#00679e',
 		}
 	},
 	computed: {
@@ -74,27 +86,53 @@ export default {
 }
 </script>
 
-<style scoped>
-.setup__form-content {
+<style lang="scss" scoped>
+@use '../../css/fluid';
+
+.setup {
+	/* make sure the background image covers everything */
 	min-height: 100%;
-}
+	min-width: 100%;
 
-/* overrides for custom icon size and full opacity */
-:deep(.empty-content__icon) {
-	width: 128px !important;
-	height: 128px !important;
-	opacity: 1 !important;
+	@include fluid.background;
 
-	.setup__form-content__svg-wrapper {
-		width: 128px;
-		height: 128px;
+	/* put the contents to the center */
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	/** fallback gradient when the theme color isn't standard blue */
+	&--themed {
+		@include fluid.gradient-background;
 	}
 
-	:deep(svg) {
-		width: 128px !important;
-		height: 128px !important;
-		max-width: 128px !important;
-		max-height: 128px !important;
+	&__form-content {
+		/** make it as narrow as possible */
+		flex-grow: 0;
+
+		background-color: var(--color-main-background-blur);
+		padding: calc(3 * var(--default-grid-baseline));
+		border-radius: var(--border-radius-container);
+		box-shadow: 0 0 10px var(--color-box-shadow);
+
+		/* overrides for custom icon size and full opacity */
+		:deep(.empty-content__icon) {
+			width: 128px !important;
+			height: 128px !important;
+			opacity: 1 !important;
+
+			.setup__form-content__svg-wrapper {
+				width: 128px;
+				height: 128px;
+
+				svg {
+					width: 128px !important;
+					height: 128px !important;
+					max-width: 128px !important;
+					max-height: 128px !important;
+				}
+			}
+		}
 	}
 }
 
