c92b3ef0870bf4637dd02dbf505db27c0a297e70
Biontium <johnsonpe83@gmail.com>
2025-09-12 18:38:38 +0200

    fix: restore color menu in text/background selector
    Removed a single line that caused the color picker menu to not display colors in the email composer.
    
    Signed-off-by: Biontium <johnsonpe83@gmail.com>

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -660,7 +660,6 @@ https://github.com/ckeditor/ckeditor5/issues/1142
 
 .ck.ck-toolbar .ck-button {
 	color: var(--color-main-text) !important;
-	background: transparent !important;
 }
 
 .ck.ck-toolbar .ck-button:hover,
