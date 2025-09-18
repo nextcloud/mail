f76c3a2b6aeba3bf5c177c1cf0892ef414a9f946
Daniel Kesselberg <mail@danielkesselberg.de>
2025-09-18 12:27:12 +0200

    fix: error in mounted hook
    The error `Error in mounted hook (Promise/async): "TypeError: this.mainStore.fetchAllQuickActions is not a function"` prevents hasCurrentUserPrincipalAndCollectionsMutation becoming true and thus the imip preview do not work.
    
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,6 @@ export default {
 		this.sync()
 		await this.mainStore.fetchCurrentUserPrincipal()
 		await this.mainStore.loadCollections()
-		await this.mainStore.fetchAllQuickActions()
 		this.mainStore.hasCurrentUserPrincipalAndCollectionsMutation(true)
 	},
 	methods: {
