05da3da3d75721f8a4a967300cbf0ca7928f1faa
Richard Steinmetz <richard@steinmetz.cloud>
2025-10-10 13:19:08 +0200

    chore(webpack): migrate to shared config
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- templates/settings-admin.php
+++ templates/settings-admin.php
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-script(\OCA\Mail\AppInfo\Application::APP_ID, 'settings');
+script(\OCA\Mail\AppInfo\Application::APP_ID, 'nextcloud-mail-settings');
 
 ?>
 <div id="mail-admin-settings">
