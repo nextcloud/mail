b44bcee43fdfdd741eef0171850869b37369b9c9
Carl Schwan <carl.schwan@nextcloud.com>
2025-11-19 17:25:27 +0100

    refactor(rector): Update rector and add nextcloud set
    Signed-off-by: Carl Schwan <carl.schwan@nextcloud.com>

--- rector.php
+++ rector.php
@@ -7,8 +7,10 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+use Nextcloud\Rector\Set\NextcloudSets;
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\Class_\AddTestsVoidReturnTypeWhereNoReturnRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 
 return RectorConfig::configure()
 	->withPaths([
@@ -16,14 +18,20 @@ return RectorConfig::configure()
 		__DIR__ . '/tests',
 	])
 	->withSkip([
-		__DIR__ . '/lib/Vendor'
+		__DIR__ . '/lib/Vendor',
+		__DIR__ . '/tests/stubs',
+	])
+	->withSets([
+		NextcloudSets::NEXTCLOUD_27,
+		PHPUnitSetList::PHPUNIT_120
 	])
 	->withPreparedSets(
+		deadCode: true,
 		phpunitCodeQuality: true,
-	)
-	->withPhpSets(
-		php74: true,
-	)
-	->withRules([
-		AddTestsVoidReturnTypeWhereNoReturnRector::class,
+		typeDeclarations: true,
+	)->withPhpSets(
+		php81: true,
+	)->withConfiguredRule(ClassPropertyAssignToConstructorPromotionRector::class, [
+		'inline_public' => true,
+		'rename_property' => true,
 	]);
