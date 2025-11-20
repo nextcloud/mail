5f394f571bf971d943d302cc98ccb248a12dd0ad
Daniel Kesselberg <mail@danielkesselberg.de>
2025-11-20 21:41:46 +0100

    fix(sieve): ensure the stop action is always last
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- src/components/mailFilter/UpdateModal.vue
+++ src/components/mailFilter/UpdateModal.vue
@@ -147,6 +147,16 @@ export default {
 
 		createAction() {
 			this.clone.actions.push({ id: randomId(), type: 'fileinto' })
+			this.clone.actions.sort((a, b) => {
+				// ensure the stop action is always last
+				if (a.id === 'stop') {
+					return 1
+				}
+				if (b.id === 'stop') {
+					return -1
+				}
+				return 0
+			})
 		},
 
 		updateAction(action) {
