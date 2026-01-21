b80ab5eab6ac12ec477b0d55bfe29c57c6e7d1d3
Hamza <hamzamahjoubi221@gmail.com>
2026-01-21 15:10:47 +0100

    fix: clean text blocks on user deletion
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- tests/Unit/Service/TextBlockServiceTest.php
+++ tests/Unit/Service/TextBlockServiceTest.php
@@ -349,5 +349,12 @@ final class TextBlockServiceTest extends TestCase {
 		$this->assertSame($shares, $result);
 	}
 
+	public function testDeleteByUserId(): void {
+		$userId = 'toBeDeleted';
+		$this->textBlockMapper->expects($this->once())
+			->method('deleteByUserId')
+			->with($userId);
+		$this->textBlockService->deleteByUserId($userId);
+	}
 
 }
