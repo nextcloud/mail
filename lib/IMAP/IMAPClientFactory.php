60bff2595212855e1894ef0655ba3f408c451412
Timo Nieminen <timo.nieminen@tnnet.fi>
2026-01-24 07:11:16 +0000

    feat(provisioning): add support for Dovecot master user authentication with new columns and UI updates
    Signed-off-by: Timo Nieminen <timo.nieminen@tnnet.fi>

--- lib/IMAP/IMAPClientFactory.php
+++ lib/IMAP/IMAPClientFactory.php
@@ -14,6 +14,7 @@ use Horde_Imap_Client_Password_Xoauth2;
 use Horde_Imap_Client_Socket;
 use OCA\Mail\Account;
 use OCA\Mail\Cache\HordeCacheFactory;
+use OCA\Mail\Db\ProvisioningMapper;
 use OCA\Mail\Events\BeforeImapClientCreated;
 use OCA\Mail\Exception\ServiceException;
 use OCP\AppFramework\Utility\ITimeFactory;
@@ -27,120 +28,133 @@ use function implode;
 use function json_encode;
 
 class IMAPClientFactory {
-	/** @var ICrypto */
-	private $crypto;
+    /** @var ICrypto */
+    private $crypto;
 
-	/** @var IConfig */
-	private $config;
+    /** @var IConfig */
+    private $config;
 
-	/** @var ICacheFactory */
-	private $cacheFactory;
+    /** @var ICacheFactory */
+    private $cacheFactory;
 
-	/** @var IEventDispatcher */
-	private $eventDispatcher;
+    /** @var IEventDispatcher */
+    private $eventDispatcher;
 
-	private ITimeFactory $timeFactory;
-	private HordeCacheFactory $hordeCacheFactory;
+    private ITimeFactory $timeFactory;
+    private HordeCacheFactory $hordeCacheFactory;
+    private ProvisioningMapper $provisioningMapper;
 
-	public function __construct(ICrypto $crypto,
-		IConfig $config,
-		ICacheFactory $cacheFactory,
-		IEventDispatcher $eventDispatcher,
-		ITimeFactory $timeFactory,
-		HordeCacheFactory $hordeCacheFactory) {
-		$this->crypto = $crypto;
-		$this->config = $config;
-		$this->cacheFactory = $cacheFactory;
-		$this->eventDispatcher = $eventDispatcher;
-		$this->timeFactory = $timeFactory;
-		$this->hordeCacheFactory = $hordeCacheFactory;
-	}
+    public function __construct(ICrypto $crypto,
+        IConfig $config,
+        ICacheFactory $cacheFactory,
+        IEventDispatcher $eventDispatcher,
+        ITimeFactory $timeFactory,
+        HordeCacheFactory $hordeCacheFactory,
+        ProvisioningMapper $provisioningMapper) {
+        $this->crypto = $crypto;
+        $this->config = $config;
+        $this->cacheFactory = $cacheFactory;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->timeFactory = $timeFactory;
+        $this->hordeCacheFactory = $hordeCacheFactory;
+        $this->provisioningMapper = $provisioningMapper;
+    }
 
-	/**
-	 * Get the connection object for the given account
-	 *
-	 * Connections are not closed until destruction, so the caller site is
-	 * responsible to log out as soon as possible to keep the number of open
-	 * (and stale) connections at a minimum.
-	 *
-	 * @param Account $account
-	 * @param bool $useCache
-	 *
-	 * @return Horde_Imap_Client_Socket
-	 * @throws ServiceException
-	 */
-	public function getClient(Account $account, bool $useCache = true): Horde_Imap_Client_Socket {
-		$this->eventDispatcher->dispatchTyped(
-			new BeforeImapClientCreated($account)
-		);
-		$host = $account->getMailAccount()->getInboundHost();
-		$user = $account->getMailAccount()->getInboundUser();
-		$decryptedPassword = null;
-		if ($account->getMailAccount()->getInboundPassword() !== null) {
-			$decryptedPassword = $this->crypto->decrypt($account->getMailAccount()->getInboundPassword());
-		}
-		$port = $account->getMailAccount()->getInboundPort();
-		$sslMode = $account->getMailAccount()->getInboundSslMode();
-		if ($sslMode === 'none') {
-			$sslMode = false;
-		}
+    /**
+     * Get the connection object for the given account
+     *
+     * Connections are not closed until destruction, so the caller site is
+     * responsible to log out as soon as possible to keep the number of open
+     * (and stale) connections at a minimum.
+     *
+     * @param Account $account
+     * @param bool $useCache
+     *
+     * @return Horde_Imap_Client_Socket
+     * @throws ServiceException
+     */
+    public function getClient(Account $account, bool $useCache = true): Horde_Imap_Client_Socket {
+        $this->eventDispatcher->dispatchTyped(
+            new BeforeImapClientCreated($account)
+        );
+        $host = $account->getMailAccount()->getInboundHost();
+        $user = $account->getMailAccount()->getInboundUser();
+        $decryptedPassword = null;
+        if ($account->getMailAccount()->getInboundPassword() !== null) {
+            $decryptedPassword = $this->crypto->decrypt($account->getMailAccount()->getInboundPassword());
+        }
+        $port = $account->getMailAccount()->getInboundPort();
+        $sslMode = $account->getMailAccount()->getInboundSslMode();
+        if ($sslMode === 'none') {
+            $sslMode = false;
+        }
 
-		$params = [
-			'username' => $user,
-			'password' => $decryptedPassword,
-			'hostspec' => $host,
-			'port' => $port,
-			'secure' => $sslMode,
-			'timeout' => (int)$this->config->getSystemValue('app.mail.imap.timeout', 5),
-			'context' => [
-				'ssl' => [
-					'verify_peer' => $this->config->getSystemValueBool('app.mail.verify-tls-peer', true),
-					'verify_peer_name' => $this->config->getSystemValueBool('app.mail.verify-tls-peer', true),
-				],
-			],
-		];
-		if ($account->getMailAccount()->getAuthMethod() === 'xoauth2') {
-			try {
-				$oauthAccessToken = $account->getMailAccount()->getOauthAccessToken();
-				if ($oauthAccessToken === null) {
-					throw new ServiceException('Missing access token for xoauth2 account');
-				}
-				$decryptedAccessToken = $this->crypto->decrypt($oauthAccessToken);
-			} catch (Exception $e) {
-				throw new ServiceException('Could not decrypt account access token: ' . $e->getMessage(), 0, $e);
-			}
+        // Check for Dovecot master user authentication
+        $provisioningId = $account->getMailAccount()->getProvisioningId();
+        if ($provisioningId !== null) {
+            $provisioning = $this->provisioningMapper->get($provisioningId);
+            if ($provisioning !== null && !empty($provisioning->getMasterUser())) {
+                $separator = $provisioning->getMasterUserSeparator() ?? '*';
+                $user = $user . $separator . $provisioning->getMasterUser();
+            }
+        }
 
-			$params['password'] = $decryptedAccessToken; // Not used, but Horde wants this
-			$params['xoauth2_token'] = new Horde_Imap_Client_Password_Xoauth2(
-				$account->getEmail(),
-				$decryptedAccessToken,
-			);
-		}
-		$paramHash = hash(
-			'sha512',
-			implode('-', [
-				$this->config->getSystemValueString('secret'),
-				$account->getId(),
-				json_encode($params)
-			]),
-		);
-		if ($useCache) {
-			$params['cache'] = [
-				'backend' => $this->hordeCacheFactory->newCache($account),
-			];
-		}
-		if ($account->getDebug() || $this->config->getSystemValueBool('app.mail.debug')) {
-			$fn = 'mail-' . $account->getUserId() . '-' . $account->getId() . '-imap.log';
-			$params['debug'] = $this->config->getSystemValue('datadirectory') . '/' . $fn;
-		}
+        $params = [
+            'username' => $user,
+            'password' => $decryptedPassword,
+            'hostspec' => $host,
+            'port' => $port,
+            'secure' => $sslMode,
+            'timeout' => (int)$this->config->getSystemValue('app.mail.imap.timeout', 5),
+            'context' => [
+                'ssl' => [
+                    'verify_peer' => $this->config->getSystemValueBool('app.mail.verify-tls-peer', true),
+                    'verify_peer_name' => $this->config->getSystemValueBool('app.mail.verify-tls-peer', true),
+                ],
+            ],
+        ];
+        if ($account->getMailAccount()->getAuthMethod() === 'xoauth2') {
+            try {
+                $oauthAccessToken = $account->getMailAccount()->getOauthAccessToken();
+                if ($oauthAccessToken === null) {
+                    throw new ServiceException('Missing access token for xoauth2 account');
+                }
+                $decryptedAccessToken = $this->crypto->decrypt($oauthAccessToken);
+            } catch (Exception $e) {
+                throw new ServiceException('Could not decrypt account access token: ' . $e->getMessage(), 0, $e);
+            }
 
-		$client = new HordeImapClient($params);
+            $params['password'] = $decryptedAccessToken; // Not used, but Horde wants this
+            $params['xoauth2_token'] = new Horde_Imap_Client_Password_Xoauth2(
+                $account->getEmail(),
+                $decryptedAccessToken,
+            );
+        }
+        $paramHash = hash(
+            'sha512',
+            implode('-', [
+                $this->config->getSystemValueString('secret'),
+                $account->getId(),
+                json_encode($params)
+            ]),
+        );
+        if ($useCache) {
+            $params['cache'] = [
+                'backend' => $this->hordeCacheFactory->newCache($account),
+            ];
+        }
+        if ($account->getDebug() || $this->config->getSystemValueBool('app.mail.debug')) {
+            $fn = 'mail-' . $account->getUserId() . '-' . $account->getId() . '-imap.log';
+            $params['debug'] = $this->config->getSystemValue('datadirectory') . '/' . $fn;
+        }
 
-		$rateLimitingCache = $this->cacheFactory->createDistributed('mail_imap_ratelimit');
-		if ($rateLimitingCache instanceof IMemcache) {
-			$client->enableRateLimiter($rateLimitingCache, $paramHash, $this->timeFactory);
-		}
+        $client = new HordeImapClient($params);
 
-		return $client;
-	}
+        $rateLimitingCache = $this->cacheFactory->createDistributed('mail_imap_ratelimit');
+        if ($rateLimitingCache instanceof IMemcache) {
+            $client->enableRateLimiter($rateLimitingCache, $paramHash, $this->timeFactory);
+        }
+
+        return $client;
+    }
 }
