2f55e70baeb2c29bef7345d37b5c357e5d0e14c1
David Dreschner <david.dreschner@nextcloud.com>
2026-06-09 15:22:19 +0200

    feat(UserMigration): Overwork migration to include all settings (basic changes)
    Signed-off-by: David Dreschner <david.dreschner@nextcloud.com>

--- src/tests/unit/service/MessageService.spec.js
+++ src/tests/unit/service/MessageService.spec.js
@@ -7,7 +7,15 @@ import axios from '@nextcloud/axios'
 import { generateUrl } from '@nextcloud/router'
 import * as MessageService from '../../../service/MessageService.js'
 
-vi.mock('@nextcloud/axios')
+vi.mock('@nextcloud/axios', () => ({
+	default: {
+		get: vi.fn(),
+		post: vi.fn(),
+		put: vi.fn(),
+		patch: vi.fn(),
+		delete: vi.fn(),
+	},
+}))
 vi.mock('@nextcloud/router')
 
 describe('service/MessageService test suite', () => {
