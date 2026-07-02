32767031a72ee576aff1325c05a81fdab16be8b5
steven-mpawulo <stevenmpawulo@gmail.com>
2026-07-02 09:41:16 +0300

    refactor: adjust removal of listerners

--- src/components/NewMessageModal.vue
+++ src/components/NewMessageModal.vue
@@ -631,9 +631,6 @@ export default {
 			this.mainStore.setComposerIndicatorDisabledMutation(true)
 			await this.onMinimize()
 
-			window.removeEventListener('beforeunload', this.onBeforeUnload)
-			window.removeEventListener('resize', this.checkScreenSize)
-
 			// End the session only if all unsaved changes have been saved
 			if (this.canSaveDraft && ((this.changed && this.draftSaved) || !this.changed)) {
 				logger.debug('Closing composer session due to close button click')
@@ -643,6 +640,9 @@ export default {
 					id: this.composerData.id,
 				})
 			}
+
+			window.removeEventListener('beforeunload', this.onBeforeUnload)
+			window.removeEventListener('resize', this.checkScreenSize)
 		},
 	},
 }
