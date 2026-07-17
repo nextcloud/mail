db2ba8c7d881c89b4b8c21be85fbe6287a81ab6a
Jana Peper <jana.peper@nextcloud.com>
2026-07-17 14:29:06 +0200

    fix: get message id instead of uid
    Signed-off-by: Jana Peper <jana.peper@nextcloud.com>

--- lib/Events/MessageFlaggedEvent.php
+++ lib/Events/MessageFlaggedEvent.php
@@ -11,14 +11,20 @@ namespace OCA\Mail\Events;
 
 use OCA\Mail\Account;
 use OCA\Mail\Db\Mailbox;
+use OCA\Mail\IMAP\IMAPClientFactory;
+use OCA\Mail\IMAP\MessageMapper as ImapMessageMapper;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IWebhookCompatibleEvent;
 
+
 class MessageFlaggedEvent extends Event implements IWebhookCompatibleEvent {
 	public function __construct(
+		private ImapMessageMapper $imapMessageMapper,
+		private IMAPClientFactory $imapClientFactory,
 		private Account $account,
 		private Mailbox $mailbox,
 		private int $uid,
+		private int $messageId,
 		private string $flag,
 		private bool $set,
 	) {
@@ -37,6 +43,17 @@ class MessageFlaggedEvent extends Event implements IWebhookCompatibleEvent {
 		return $this->uid;
 	}
 
+	public function getMessageId(): ?string {
+		$client = $this->imapClientFactory->getClient($this->account);
+		$message = $this->imapMessageMapper->find(
+				$client,
+				$this->mailbox->getName(),
+				$this->uid,
+				$this->account->getUserId()
+			);
+		return $message->getMessageId();
+	}
+
 	public function getFlag(): string {
 		return $this->flag;
 	}
@@ -49,7 +66,7 @@ class MessageFlaggedEvent extends Event implements IWebhookCompatibleEvent {
 		return [
 			'accountId' => $this->account->getId(),
 			'mailboxId' => $this->mailbox->getId(),
-			'messageUid' => $this->uid,
+			'messageUid' => $this->getMessageId(),
 			'flag' => $this->flag,
 			'set' => $this->set,
 		];
