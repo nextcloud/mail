2084981cafde45426bbaeae977fd6d3985a0d926
SebastianKrupinski <krupinskis05@gmail.com>
2026-08-10 17:34:47 -0400

    fixup! feat: jmap support - part 2
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- lib/Contracts/IMailboxConnector.php
+++ lib/Contracts/IMailboxConnector.php
@@ -11,16 +11,21 @@ namespace OCA\Mail\Contracts;
 
 use OCA\Mail\Account;
 use OCA\Mail\Db\Mailbox;
+use OCA\Mail\Exception\ServiceException;
 
 interface IMailboxConnector {
 
 	/**
 	 * Synchronize the list of mailboxes for the given account.
+	 *
+	 * @throws ServiceException
 	 */
 	public function syncAll(Account $account, bool $force = false): void;
 
 	/**
 	 * Refresh cached statistics (total / unseen) for a single mailbox.
+	 *
+	 * @throws ServiceException
 	 */
 	public function syncOne(Account $account, Mailbox $mailbox): void;
 
@@ -28,21 +33,29 @@ interface IMailboxConnector {
 	 * Create a new mailbox on the remote server and persist it locally.
 	 *
 	 * @param string[] $specialUse
+	 *
+	 * @throws ServiceException
 	 */
 	public function create(Account $account, string $name, array $specialUse = []): Mailbox;
 
 	/**
 	 * Rename an existing mailbox on the remote server.
+	 *
+	 * @throws ServiceException
 	 */
 	public function rename(Account $account, Mailbox $mailbox, string $newName): Mailbox;
 
 	/**
 	 * Delete a mailbox on the remote server and remove it locally.
+	 *
+	 * @throws ServiceException
 	 */
 	public function delete(Account $account, Mailbox $mailbox): void;
 
 	/**
 	 * Subscribe or unsubscribe a mailbox.
+	 *
+	 * @throws ServiceException
 	 */
 	public function subscribe(Account $account, Mailbox $mailbox, bool $subscribed): Mailbox;
 }
--- lib/Service/MailManager.php
+++ lib/Service/MailManager.php
@@ -9,9 +9,6 @@ declare(strict_types=1);
 
 namespace OCA\Mail\Service;
 
-use Horde_Imap_Client_Exception;
-use Horde_Imap_Client_Exception_NoSupportExtension;
-use Horde_Mime_Exception;
 use OCA\Mail\Account;
 use OCA\Mail\Attachment;
 use OCA\Mail\Db\Mailbox;
@@ -28,9 +25,9 @@ use OCA\Mail\Events\MessageFlaggedEvent;
 use OCA\Mail\Exception\ClientException;
 use OCA\Mail\Exception\ImapFlagEncodingException;
 use OCA\Mail\Exception\ServiceException;
+use OCA\Mail\Exception\SmimeDecryptException;
 use OCA\Mail\Exception\TrashMailboxNotSetException;
 use OCA\Mail\IMAP\ImapFlag;
-use OCA\Mail\IMAP\MessageMapper as ImapMessageMapper;
 use OCA\Mail\Model\IMAPMessage;
 use OCA\Mail\Protocol\ProtocolFactory;
 use OCA\Mail\Service\Search\SearchQuery;
@@ -44,7 +41,6 @@ class MailManager {
 
 	public function __construct(
 		private readonly MailboxMapper $mailboxMapper,
-		private readonly ImapMessageMapper $imapMessageMapper,
 		private readonly DbMessageMapper $dbMessageMapper,
 		private readonly IEventDispatcher $eventDispatcher,
 		private readonly LoggerInterface $logger,
@@ -58,6 +54,9 @@ class MailManager {
 
 	//** ============================ Mailbox Operations ============================ */
 
+	/**
+	 * @throws ClientException
+	 */
 	public function getMailbox(string $uid, int $id): Mailbox {
 		try {
 			return $this->mailboxMapper->findByUid($id, $uid);
@@ -81,30 +80,48 @@ class MailManager {
 		return $this->mailboxMapper->findAll($account);
 	}
 
+	/**
+	 * @throws ServiceException
+	 * @throws DoesNotExistException the mailbox could not be re-read after being created on the remote (IMAP-backed accounts only)
+	 */
 	public function createMailbox(Account $account, string $name, array $specialUse = []): Mailbox {
 		return $this->protocolFactory
 			->mailboxConnector($account)
 			->create($account, $name, $specialUse);
 	}
 
+	/**
+	 * @throws ServiceException
+	 */
 	public function renameMailbox(Account $account, Mailbox $mailbox, string $name): Mailbox {
 		return $this->protocolFactory
 			->mailboxConnector($account)
 			->rename($account, $mailbox, $name);
 	}
 
+	/**
+	 * @throws ServiceException
+	 */
 	public function deleteMailbox(Account $account, Mailbox $mailbox): void {
 		$this->protocolFactory
 			->mailboxConnector($account)
 			->delete($account, $mailbox);
 	}
 
+	/**
+	 * @throws ServiceException
+	 * @throws DoesNotExistException the mailbox could not be re-read after being updated on the remote (IMAP-backed accounts only)
+	 */
 	public function updateSubscription(Account $account, Mailbox $mailbox, bool $subscribed): Mailbox {
 		return $this->protocolFactory
 			->mailboxConnector($account)
 			->subscribe($account, $mailbox, $subscribed);
 	}
 
+	/**
+	 * @throws ClientException
+	 * @throws ServiceException
+	 */
 	public function clearMailbox(Account $account, Mailbox $mailbox): void {
 		$uids = $this->dbMessageMapper->findAllUids($mailbox);
 		if ($uids === []) {
@@ -117,10 +134,18 @@ class MailManager {
 
 	//** ============================ Message Operations ============================ */
 
+	/**
+	 * @throws DoesNotExistException
+	 */
 	public function getMessage(string $uid, int $id): Message {
 		return $this->dbMessageMapper->findByUserId($uid, $id);
 	}
 
+	/**
+	 * @throws ClientException
+	 * @throws ServiceException
+	 * @throws SmimeDecryptException
+	 */
 	public function getImapMessage(Account $account, Mailbox $mailbox, Message $message, bool $loadBody = false): IMAPMessage {
 		$messages = $this->protocolFactory
 			->messageConnector($account)
@@ -135,9 +160,11 @@ class MailManager {
 	/**
 	 * @param Account $account
 	 * @param Mailbox $mailbox
-	 * @param int[] $uids
+	 * @param bool $loadBody
+	 * @param Message ...$messages
 	 * @return IMAPMessage[]
 	 * @throws ServiceException
+	 * @throws SmimeDecryptException
 	 */
 	public function getImapMessages(Account $account, Mailbox $mailbox, bool $loadBody = false, Message ...$messages): array {
 		return $this->protocolFactory
@@ -152,6 +179,7 @@ class MailManager {
 	 *
 	 * @return string|null
 	 *
+	 * @throws ClientException
 	 * @throws ServiceException
 	 */
 	public function getRawMessage(Account $account, Mailbox $mailbox, Message $message): ?string {
@@ -177,6 +205,7 @@ class MailManager {
 
 	/**
 	 * @return int[]
+	 * @throws ServiceException
 	 */
 	public function findMessages(Account $account, Mailbox $mailbox, SearchQuery $searchQuery): array {
 		return $this->protocolFactory
@@ -186,10 +215,10 @@ class MailManager {
 
 	/**
 	 * @param Account $sourceAccount
-	 * @param string $sourceFolderId
-	 * @param int $uid
+	 * @param Mailbox $sourceMailbox
+	 * @param Message $message
 	 * @param Account $destinationAccount
-	 * @param string $destFolderId
+	 * @param Mailbox $destinationMailbox
 	 *
 	 * @return ?int the new UID (or null if couldn't be determined)
 	 * @throws ServiceException
@@ -204,6 +233,9 @@ class MailManager {
 		return $mutatedUids[0] ?? null;
 	}
 
+	/**
+	 * @throws ServiceException
+	 */
 	public function moveMessages(Account $account, Mailbox $targetMailbox, Mailbox $sourceMailbox, Message ...$messages): array {
 		if ($messages === []) {
 			return [];
@@ -233,6 +265,10 @@ class MailManager {
 		$this->deleteMessages($account, $mailbox, $message);
 	}
 
+	/**
+	 * @throws ClientException
+	 * @throws ServiceException
+	 */
 	public function deleteMessages(Account $account, Mailbox $sourceMailbox, Message ...$messages): void {
 		if ($messages === []) {
 			return;
@@ -281,6 +317,9 @@ class MailManager {
 		}
 	}
 
+	/**
+	 * @throws ServiceException
+	 */
 	public function flagMessages(Account $account, Mailbox $mailbox, string $flag, bool $value, Message ...$messages): void {
 		if ($messages === []) {
 			return;
@@ -295,10 +334,13 @@ class MailManager {
 
 		// dispatch events
 		foreach ($mutatedMessages as $message) {
-			$this->eventDispatcher->dispatchTyped(new MessageFlaggedEvent($account, $mailbox, $message->getUid(), $flag, $value));
+			$this->eventDispatcher->dispatchTyped(new MessageFlaggedEvent($account, $mailbox, $message, $flag, $value));
 		}
 	}
 
+	/**
+	 * @throws ServiceException
+	 */
 	public function tagMessages(Account $account, Mailbox $mailbox, Tag $tag, bool $value, Message ...$messages): void {
 		if ($messages === []) {
 			return;
@@ -312,6 +354,9 @@ class MailManager {
 		$this->dbMessageMapper->updateBulk($account, true, ...$mutatedMessages);
 	}
 
+	/**
+	 * @throws ServiceException
+	 */
 	public function markFolderAsRead(Account $account, Mailbox $mailbox): void {
 		// find all messages in mailbox with their remote ids
 		$messages = $this->dbMessageMapper->findByUids($mailbox, $this->dbMessageMapper->findAllUids($mailbox));
@@ -329,6 +374,7 @@ class MailManager {
 	 * Finds all messages in the thread of the given thread root id
 	 *
 	 * @return array<string, Message> array of messages in the thread, keyed by remote id
+	 * @throws DoesNotExistException
 	 */
 	public function fetchThread(Account $account, Mailbox $mailbox, string $threadRootId): array {
 		$mailAccount = $account->getMailAccount();
@@ -358,6 +404,10 @@ class MailManager {
 		return $messages;
 	}
 
+	/**
+	 * @throws DoesNotExistException
+	 * @throws ServiceException
+	 */
 	public function moveThread(Account $srcAccount, Mailbox $srcMailbox, Account $dstAccount, Mailbox $dstMailbox, string $threadRootId): array {
 		if ($srcAccount->getId() !== $dstAccount->getId()) {
 			throw new ServiceException('It is not possible to move across accounts yet');
@@ -373,6 +423,7 @@ class MailManager {
 
 	/**
 	 * @throws ClientException
+	 * @throws DoesNotExistException
 	 * @throws ServiceException
 	 */
 	public function deleteThread(Account $account, Mailbox $mailbox, string $threadRootId): void {
@@ -393,6 +444,9 @@ class MailManager {
 	 * @param Mailbox $mailbox
 	 * @param Message $message
 	 * @return Attachment[]
+	 *
+	 * @throws DoesNotExistException
+	 * @throws ServiceException
 	 */
 	public function getMailAttachments(Account $account, Mailbox $mailbox, Message $message): array {
 		return $this->protocolFactory
@@ -408,10 +462,7 @@ class MailManager {
 	 * @return Attachment
 	 *
 	 * @throws DoesNotExistException
-	 * @throws Horde_Imap_Client_Exception
-	 * @throws Horde_Imap_Client_Exception_NoSupportExtension
 	 * @throws ServiceException
-	 * @throws Horde_Mime_Exception
 	 */
 	public function getMailAttachment(Account $account, Mailbox $mailbox, Message $message, string $attachmentId): Attachment {
 		return $this->protocolFactory
@@ -424,6 +475,8 @@ class MailManager {
 	 *
 	 * @return Quota|null
 	 * @see https://tools.ietf.org/html/rfc2087
+	 *
+	 * @throws ServiceException
 	 */
 	public function getQuota(Account $account): ?Quota {
 		return $this->protocolFactory
@@ -437,6 +490,8 @@ class MailManager {
 	 * @param Account $account
 	 * @param Mailbox $mailbox
 	 * @return boolean
+	 *
+	 * @throws ServiceException
 	 */
 	public function isPermflagsEnabled(Account $account, Mailbox $mailbox): bool {
 		return $this->protocolFactory
@@ -458,6 +513,9 @@ class MailManager {
 		}
 	}
 
+	/**
+	 * @throws ClientException
+	 */
 	public function createTag(string $displayName, string $color, string $userId): Tag {
 		try {
 			$imapLabel = $this->imapFlag->create($displayName);
@@ -485,6 +543,9 @@ class MailManager {
 		return $this->tagMapper->insert($tag);
 	}
 
+	/**
+	 * @throws ClientException
+	 */
 	public function updateTag(int $id, string $displayName, string $color, string $userId): Tag {
 		try {
 			$tag = $this->tagMapper->getTagForUser($id, $userId);
@@ -502,6 +563,9 @@ class MailManager {
 	 * @param int $id tag id
 	 * @param string $userId user id of the tag owner
 	 * @param Account[] $accounts accounts to remove the tag from
+	 *
+	 * @throws ClientException
+	 * @throws ServiceException
 	 */
 	public function deleteTag(int $id, string $userId, array $accounts): Tag {
 		try {
@@ -544,16 +608,6 @@ class MailManager {
 		}
 
 		return $this->tagMapper->delete($tag);
-	}
-
-	// ============================ Helpers ============================ */
-
-	private function mapMailboxesById(array $mailboxes): array {
-		$mailboxesById = [];
-		foreach ($mailboxes as $mailbox) {
-			$mailboxesById[$mailbox->getId()] = $mailbox;
-		}
-		return $mailboxesById;
 	}
 
 }
